87f03992c8d92695d54c236cc70f5a285c6ea843
Luke Deen Taylor <luke@deentaylor.com>
2017-02-26 14:25:50 -0500

    Set up page title changes for routing

--- app/src/main.js
+++ app/src/main.js
@@ -8,10 +8,12 @@ const Home = require('./pages/home/home.vue');
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home, meta: { title: 'Codus' } },
   ],
 });
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || 'Codus';
+  next();
+});
 
-window.app = new Vue({
-  router,
-}).$mount('#app');
+window.app = new Vue({ router }).$mount('#app');
