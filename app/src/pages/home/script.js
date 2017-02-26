f3abd3cc30a34535674f4844200255025e450220
Luke Deen Taylor <luke@deentaylor.com>
2017-02-26 17:42:56 -0500

    Remove unused data() block

--- app/src/pages/home/script.js
+++ app/src/pages/home/script.js
@@ -3,10 +3,6 @@ import Starfield from '../../scripts/starfield';
 export default {
   name: 'home',
 
-  data() {
-    return { a: 'b' };
-  },
-
   mounted() {
     this.stars = new Starfield(document.getElementById('stars'));
   },
