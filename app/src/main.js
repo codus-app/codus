ad6a076a19e133109a7f95f63b25c92f25e51ef4
Luke Deen Taylor <luke@deentaylor.com>
2017-02-26 16:58:09 -0500

    Basic Auth0 setup

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,8 @@
 const Vue = require('vue');
 const VueRouter = require('vue-router');
 
+const auth = require('./scripts/auth');
+
 Vue.use(VueRouter);
 
 // Vue router setup
@@ -20,4 +22,21 @@ router.beforeEach((to, from, next) => {
 
 // Create main Vue instance
 
-window.app = new Vue({ router }).$mount('#app');
+window.app = new Vue({
+  router,
+  data: () => ({
+    authenticated: false,
+    secretThing: '',
+    lock: auth.lock,
+  }),
+
+  ready() {
+    this.authenticated = auth.checkAuth();
+  },
+
+  methods: {
+    login: auth.login,
+    logout: auth.logout,
+    checkAuth: auth.checkAuth,
+  },
+}).$mount('#app');
---
+++ app/src/scripts/auth.js
@@ -0,0 +1,31 @@
+const lock = new window.Auth0Lock('y4m8JcL7boD2FKwH3fwTS9GusF07z4IT', 'codus.auth0.com');
+
+// Callback on login
+lock.on('authenticated', (authResult) => {
+  console.log('authenticated');
+  // Store ID token in localStorage
+  localStorage.setItem('id_token', authResult.idToken);
+  // Get profile object from ID token
+  lock.getProfile(authResult.idToken, (error, profile) => {
+    if (error) return;
+    // Store profile in localStorage
+    localStorage.setItem('profile', JSON.stringify(profile));
+  });
+
+  window.app.authenticated = true;
+});
+
+module.exports = {
+  lock,
+  checkAuth: () => localStorage.getItem('id_token') !== null,
+
+  login() {
+    lock.show();
+  },
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('profile');
+    window.app.authenticated = false;
+  },
+};
