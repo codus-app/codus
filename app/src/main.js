8596bb1d16a1a4c1f6c535b25149b60de9703278
Luke Deen Taylor <luke@deentaylor.com>
2017-02-26 15:22:56 -0500

    Add name to routes

--- app/src/main.js
+++ app/src/main.js
@@ -3,17 +3,21 @@ const VueRouter = require('vue-router');
 
 Vue.use(VueRouter);
 
+// Vue router setup
+
 const Home = require('./pages/home/home.vue');
 
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home, meta: { title: 'Codus' } },
+    { name: 'home', path: '/', component: Home, meta: { title: 'Codus' } },
   ],
 });
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title || 'Codus';
   next();
 });
+
+// Create main Vue instance
 
 window.app = new Vue({ router }).$mount('#app');
