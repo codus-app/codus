b0c443d1ec7b5fda153a72d7fd8a3002a2c77e11
Luke Deen Taylor <luke@deentaylor.com>
2017-02-28 16:38:02 -0500

    Add light and dark modes to top-bar

--- app/src/components/top-bar/top-bar.vue
+++ app/src/components/top-bar/top-bar.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="top-bar">
+  <div class="top-bar" v-bind:class="{ dark }">
     <slot></slot>
   </div>
 </template>
 
 <script>
-  export default {};
+  export default {
+    data: () => ({
+      dark: false
+    }),
+    methods: {
+      toggleDark() {
+        this.dark = !this.dark;
+      }
+    },
+  };
 </script>
 
 <style scoped lang="sass" src="./style.sass"></style>
