c72585a06fb35c9965d9b987583713e50878dd1d
Luke Deen Taylor <luke@deentaylor.com>
2017-03-15 22:19:14 -0400

    add sidebar

--- app/src/pages/app/home/home.vue
+++ app/src/pages/app/home/home.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="component-root"></div>
+  <div>Well hello there</div>
 </template>
 
 <script>
 export default {};
 </script>
-
-<style scoped lang="sass" src="./style.sass"></style>
---
+++ app/src/pages/app/sidebar/sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-link">
+      <i class="material-icons">home</i> Home
+    </div>
+    <div class="sidebar-link">
+      <i class="material-icons">code</i> Practice
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="sass" src="./style.sass"></style>
