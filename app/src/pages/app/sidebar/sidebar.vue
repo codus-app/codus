f7848e3e63d1ecc4d7ac119040e01987beb43648
Luke Deen Taylor <luke@deentaylor.com>
2017-03-16 22:27:42 -0400

    Implement routing within app

--- app/src/pages/app/sidebar/sidebar.vue
+++ app/src/pages/app/sidebar/sidebar.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="sidebar">
-    <div class="profile">
-      <img src="https://arkisio.now.sh/profile/Conqueror1776" class="profile-image">
-      <div class="profile-name">Elijah Tamarchenko</div>
+  <div class="root">
+    <div class="sidebar">
+      <div class="profile">
+        <img src="https://arkisio.now.sh/profile/Conqueror1776" class="profile-image">
+        <div class="profile-name">Elijah Tamarchenko</div>
+      </div>
+
+      <router-link class="sidebar-link" to="/app/home">
+        <i class="material-icons">home</i> Home
+      </router-link>
+      <router-link class="sidebar-link" to="/app/practice">
+        <i class="material-icons">code</i> Practice
+      </router-link>
     </div>
 
-    <router-link class="sidebar-link" to="/app/home">
-      <i class="material-icons">home</i> Home
-    </router-link>
-    <router-link class="sidebar-link" to="/app/practice">
-      <i class="material-icons">code</i> Practice
-    </router-link>
+    <router-view class="router-view"><!-- router-view class is for CSS selection --></router-view>
   </div>
 </template>
 
