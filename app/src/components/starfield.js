381fb943e24373ea5aac3f611c44681b23142092
Luke Deen Taylor <luke@deentaylor.com>
2017-03-19 12:24:36 -0400

    Set up stars array

--- app/src/components/starfield.js
+++ app/src/components/starfield.js
@@ -20,9 +20,31 @@ export default {
 
   data: () => ({
     ctx: undefined,
+    stars: [],
   }),
 
   methods: {
+    // Generate a star object
+    getStar(back) {
+      const rand = (low, high) => (Math.random() * (high - low)) + low;
+      const polToCart = (r, theta) => [
+        r * Math.cos(theta),
+        r * Math.sin(theta),
+      ];
+
+      const maxRadius = Math.min(this.$refs.stars.width, this.$refs.stars.height) / 4 / 2;
+      const pos = polToCart(
+        rand(maxRadius / 25, maxRadius),
+        rand(0, Math.PI * 2),
+      );
+
+      return {
+        x: pos[0],
+        y: pos[1],
+        z: back ? 25 : rand(0, 25),
+      };
+    },
+
     // Adjust canvas coordinate size
     resize() {
       this.$refs.stars.width = this.$refs.stars.offsetWidth;
@@ -31,8 +53,15 @@ export default {
   },
 
   mounted() {
+    // Get canvas context
     this.ctx = this.$refs.stars.getContext('2d');
+    // Set up resizing
     this.resize();
     window.addEventListener('resize', () => this.resize());
+
+    // Populate stars
+    for (let i = 0; i < this.starCount; i += 1) {
+      this.stars.push(this.getStar());
+    }
   },
 };
