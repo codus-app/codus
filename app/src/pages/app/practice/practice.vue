ad2554e5b19bc60501455702443b1585e3e4b817
Luke Deen Taylor <luke@deentaylor.com>
2017-03-19 21:35:30 -0400

    Add concrete practice route

--- app/src/pages/app/practice/practice.vue
+++ app/src/pages/app/practice/practice.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Home</div>
+  <div>Practice</div>
 </template>
 
 <script>
--- app/src/pages/app/routes.js
+++ app/src/pages/app/routes.js
@@ -3,4 +3,8 @@ export default [
     path: '/app',
     component: require('./home/home.vue'),
   },
+  {
+    path: '/app/practice',
+    component: require('./practice/practice.vue'),
+  },
 ];
