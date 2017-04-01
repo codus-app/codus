11a203ad4dbf9d4dfeaf3056aa954905f8faf291
Luke Deen Taylor <luke@deentaylor.com>
2017-03-31 23:46:45 -0400

    Use v-for in sidebar

--- app/src/components/sidebar/sidebar.vue
+++ app/src/components/sidebar/sidebar.vue
@@ -5,18 +5,19 @@
         <div class="profile-name">Elijah Tamarchenko</div>
       </div>
 
-      <router-link class="sidebar-link" to="/app" exact>
-        <i class="material-icons">home</i> Home
-      </router-link>
-      <router-link class="sidebar-link" to="/app/practice">
-        <i class="material-icons">code</i> Practice
+      <router-link class="sidebar-link" v-for="route in routes" v-bind:to="route.path" v-bind:exact="route.meta.exact || false">
+        <i class="material-icons">{{route.meta.icon}}</i> {{route.meta.name}}
       </router-link>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import routes from '../../pages/app/routes';
+
+export default {
+  data: () => ({ routes }),
+};
 </script>
 
 <style scoped lang="sass" src="./style.sass"></style>
--- app/src/pages/app/routes.js
+++ app/src/pages/app/routes.js
@@ -2,9 +2,11 @@ export default [
   {
     path: '/app',
     component: require('./home/home.vue'),
+    meta: { icon: 'home', name: 'Home', exact: true },
   },
   {
     path: '/app/practice',
     component: require('./practice/practice.vue'),
+    meta: { icon: 'code', name: 'Practice' },
   },
 ];
