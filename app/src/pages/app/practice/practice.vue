6b50e16bc56861fdd11fa3b29a77eb9ed1f91b18
Luke Deen Taylor <luke@deentaylor.com>
2017-04-11 10:53:09 -0400

    Add carousel to practice page

--- app/src/pages/app/practice/practice.vue
+++ app/src/pages/app/practice/practice.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>Practice</div>
+  <div>
+    <carousel>
+      <challenge-card v-for="i in 10">
+        <div slot="top">Test</div>
+        <div slot="title">Card {{ i }}</div>
+        <div slot="bottom">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
+      </challenge-card>
+    </carousel>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style scoped lang="sass" src="./style.sass"></style>
