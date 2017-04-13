48784e8ec313aacfa551f182c6dfc9bd4cfc7dac
Luke Deen Taylor <luke@deentaylor.com>
2017-04-13 16:17:22 -0400

    Hide CODUS title when sign up modal is shown

--- app/src/pages/landing/landing.vue
+++ app/src/pages/landing/landing.vue
@@ -5,7 +5,7 @@
       <div slot="right"><auth-buttons></auth-buttons></div>
     </top-bar>
     <transition name="modal-header-fade">
-      <h1 v-if="!$store.state.loginShown">codus</h1>
+      <h1 v-if="!($store.state.loginShown || $store.state.signupShown)">codus</h1>
     </transition>
     <starfield></starfield>
     <modal bgcolor="rgba(0, 0, 0, 0.75)" v-if="$store.state.loginShown" v-on:close="$store.commit('toggleLoginModal')">
