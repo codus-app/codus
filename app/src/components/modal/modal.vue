9f4d2fdfcffb95ab5d62074f71c77bf9717a2b7c
Luke Deen Taylor <luke@deentaylor.com>
2017-04-12 23:58:17 -0400

    Simplify modal with .click
    Misc other modal improvements

--- app/src/components/modal/modal.vue
+++ app/src/components/modal/modal.vue
@@ -1,9 +1,6 @@
 <template>
   <transition name="modal-fade">
-    <div class="modal-bg"
-         :style="{ backgroundColor: bgcolor || 'rgba(0, 0, 0, 0.5)' }"
-         @click="bgClickClose"
-         ref="modal-bg">
+    <div class="modal-bg" ref="modal-bg" v-bind:style="style" v-on:click.self="$emit('close')">
       <div class="modal-container">
         <div class="modal">
           <slot>Modal!</slot>
@@ -13,5 +10,15 @@
   </transition>
 </template>
 
-<script src="./script.js"></script>
+<script>
+export default {
+  props: ['bgcolor'],
+  computed: {
+    style() {
+      return { backgroundColor: this.bgcolor || 'rgba(0, 0, 0, 0.5)' };
+    },
+  },
+};
+</script>
+
 <style scoped lang="sass" src="./style.sass"></style>
