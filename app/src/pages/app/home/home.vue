e4de9ece62f92a97299bc3082de7db9a10ac8293
Luke Deen Taylor <luke@deentaylor.com>
2017-04-14 03:12:17 +0000

    Don't say 'welcome back' if it's the user's first time logging
    Say 'welcome' instead

--- app/src/pages/app/home/home.vue
+++ app/src/pages/app/home/home.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
     <binary-background></binary-background>
-    <h1>Welcome<br>back,<br>{{ firstName }}</h1>
+    <h1>Welcome<span v-if="loginCount > 1"><br>back</span>,<br>{{ firstName }}</h1>
   </div>
 </template>
 
 <script>
+import auth from '../../../scripts/auth';
+
 export default {
   data: () => ({
     userInfo: JSON.parse(localStorage.getItem('profile')),
+    loginCount: 0,
   }),
   computed: {
     firstName() { return this.userInfo.user_metadata.name.split(' ')[0]; },
+  },
+  created() {
+    const man = auth.getManagement();
+    man.getUser(this.userInfo.user_id, (err, rsp) => {
+      if (err) console.log(err);
+      else this.loginCount = rsp.logins_count;
+    });
   },
 };
 </script>
