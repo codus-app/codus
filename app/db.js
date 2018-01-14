2dd8a075cea64f5749cac6f5e4e9d23329441345
Luke Taylor <luke@deentaylor.com>
2018-01-14 13:37:01 -0500

    Connect to database

---
+++ app/db.js
@@ -0,0 +1,35 @@
+// Contains logic for connecting to the database
+
+const mongoose = require('mongoose');
+
+// Construct URI for connection
+
+const username = 'admin';
+const password = process.env.CODUS_DB_PASS;
+const replicas = [
+  'codus-shard-00-00-ezq3k.mongodb.net',
+  'codus-shard-00-01-ezq3k.mongodb.net',
+  'codus-shard-00-02-ezq3k.mongodb.net',
+];
+const port = 27017;
+const dbName = 'codus';
+const replicaSet = 'Codus-shard-0';
+
+const uri = `mongodb://${username}:${password}@${replicas.map(u => `${u}:${port}`).join(',')}/${dbName}`;
+
+// Connect
+
+mongoose.connect(uri, {
+  replicaSet,
+  ssl: true,
+  authSource: 'admin',
+  useMongoClient: true,
+});
+const db = mongoose.connection;
+
+
+// Throw error if we can't connect
+db.on('error', e => console.log('connection error:', e));
+// Otherwise go ahead
+db.once('open', () => {
+});
