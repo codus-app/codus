c069068b452d6eb559d4e4571c2759fc561fb3db
Luke Taylor <luke@deentaylor.com>
2018-01-17 12:56:27 -0500

    Add isValidIdentifier to java.js

--- app/java.js
+++ app/java.js
@@ -22,3 +22,27 @@ module.exports.toJs = {
 
 // Just a list of the Java type names
 module.exports.types = Array.from(Object.keys(module.exports.toJs));
+
+// Java keywords can't be used as identifiers
+// From https://docs.oracle.com/javase/specs/jls/se7/html/jls-3.html#jls-3.9
+module.exports.keywords = [
+  'abstract', 'continue', 'for', 'new', 'switch',
+  'assert', 'default', 'if', 'package', 'synchronized',
+  'boolean', 'do', 'goto', 'private', 'this',
+  'break', 'double', 'implements', 'protected', 'throw',
+  'byte', 'else', 'import', 'public', 'throws',
+  'case', 'enum', 'instanceof', 'return', 'transient',
+  'catch', 'extends', 'int', 'short', 'try',
+  'char', 'final', 'interface', 'static', 'void',
+  'class', 'finally', 'long', 'strictfp', 'volatile',
+  'const', 'float', 'native', 'super', 'while',
+];
+
+// Checks if a given string is a valid java identifier
+module.exports.isValidIdentifier = function isValidIdentifier(obj) {
+  if (typeof obj !== 'string') return false;
+  // Certain names are illegal identifiers
+  if (module.exports.keywords.includes(obj) || ['true', 'false', 'null'].includes(obj)) return false;
+  // Sticking to the enligh alphabet because there's no easy way to do it properly
+  return /^[a-zA-Z_][a-zA-Z0-9_]*$/.test(obj);
+};
