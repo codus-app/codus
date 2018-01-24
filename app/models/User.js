ac3b74ab6f6460edc92e8af5cdb9d4778dde4ec2
Luke Taylor <luke@deentaylor.com>
2018-01-24 16:24:10 -0500

    Add addSolution method to User

--- app/models/User.js
+++ app/models/User.js
@@ -31,4 +31,11 @@ const userSchema = new mongoose.Schema({
   solutions: [solutionSchema],
 });
 
+// Add a problem solution to the user's data
+userSchema.methods.addSolution = async function addSolution(problemName, code, passed) {
+  if (typeof passed === 'undefined') { /* TODO: evaluate user's solution ? */ }
+  this.solutions.push({ name: problemName, solution: code, passed });
+  this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
