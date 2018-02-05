2b3f8b2b4b0a1571b92c4fe2d2dd5e9b273dc386
Luke Taylor <luke@deentaylor.com>
2018-02-04 21:58:33 -0500

    Reorder some of the API endpoints again

--- app/index.js
+++ app/index.js
@@ -19,6 +19,22 @@ app.get('/', (req, res) => {
 });
 
 
+// Query the database for a problem
+app.get('/problem/:name', async (req, res) => {
+  // By name
+  const problem = await data.getProblem.byName(req.params.name);
+  if (!problem) res.status(404).json({ error: `Problem ${req.params.name} was not found` });
+  else res.json(stripId(problem));
+});
+
+// Get all the problems in a category
+app.get('/category/:name', async (req, res) => {
+  const problems = await data.getProblems.byCategory(req.params.name);
+  if (!problems.length) res.status(404).json({ error: `No problems match category ${req.params.name}` });
+  else res.json(stripId(problems));
+});
+
+
 // Return the user info encoded in the Authorization header
 app.get('/userinfo', auth0(), (req, res) => {
   res.json(req.user);
@@ -37,22 +53,6 @@ app.get('/solution/:problemName', auth0(), async (req, res) => {
   const solution = await user.getSolution(req.params.problemName);
   if (!solution) res.status(404).json({ error: `no solution by the authenticated user for problem ${req.params.problemName} was found` });
   else res.json(stripId(solution));
-});
-
-
-// Query the database for a problem
-app.get('/problem/:name', async (req, res) => {
-  // By name
-  const problem = await data.getProblem.byName(req.params.name);
-  if (!problem) res.status(404).json({ error: `Problem ${req.params.name} was not found` });
-  else res.json(stripId(problem));
-});
-
-// Get all the problems in a category
-app.get('/category/:name', async (req, res) => {
-  const problems = await data.getProblems.byCategory(req.params.name);
-  if (!problems.length) res.status(404).json({ error: `No problems match category ${req.params.name}` });
-  else res.json(stripId(problems));
 });
 
 
