f80685e5fbd91101a100131034ade8ecb2b0d7b1
Luke Taylor <luke@deentaylor.com>
2018-02-13 19:08:48 -0500

    Remove challenge card component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,9 +2,7 @@ import Vue from 'vue';
 
 import Sidebar from './sidebar/sidebar.vue';
 import BinaryBackground from './binary-background';
-import ChallengeCard from './challenge-card/challenge-card.vue';
 
 
 Vue.component('binary-background', BinaryBackground);
-Vue.component('challenge-card', ChallengeCard);
 Vue.component('sidebar', Sidebar);
