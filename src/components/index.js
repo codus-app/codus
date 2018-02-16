91e9a71ca69e951b1b59628a2f58c6eccbdad0e8
Luke Taylor <luke@deentaylor.com>
2018-02-16 10:35:29 -0500

    Register appropriate feather-icons as components

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 
+import { UserIcon, LockIcon } from 'vue-feather-icons';
+
 import Starfield from './starfield';
 import Modal from './modal/modal.vue';
 import Login from './login/login.vue';
 import Signup from './signup/signup.vue';
 import TopBar from './top-bar/top-bar.vue';
 import AuthButtons from './auth-buttons.vue';
+
+Vue.component('user-icon', UserIcon);
+Vue.component('lock-icon', LockIcon);
 
 Vue.component('starfield', Starfield);
 Vue.component('modal', Modal);
