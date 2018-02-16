25e747ac69ad159ef96a01a37d34cffb207f6c14
Luke Taylor <luke@deentaylor.com>
2018-02-16 14:23:31 -0500

    Add sidebar component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
 // Import components
+import Sidebar from './sidebar/sidebar.vue';
 
 // Register components
+Vue.component('sidebar', Sidebar);
---
+++ src/app/components/sidebar/sidebar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="sidebar">
+    <h1>Codus</h1>
+  </div>
+</template>
+
+<script> export default {}; </script>
+
+<style scoped lang="sass" src='./style.sass'></style>
