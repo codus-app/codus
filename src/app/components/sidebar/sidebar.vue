faef9a5cb9e0442ff5d22dcc3b5d5807c5ad6107
Luke Taylor <luke@deentaylor.com>
2018-02-16 15:04:15 -0500

    Add links section of sidebar with headings for different link categories

--- src/app/components/sidebar/sidebar.vue
+++ src/app/components/sidebar/sidebar.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="sidebar">
     <h1>Codus</h1>
+
+    <div class="links">
+
+      <!-- Personal user-related links -->
+      <h2>You</h2>
+
+      <!-- Class-related links -->
+      <h2>Class</h2>
+
+    </div>
   </div>
 </template>
 
