47a65ea2b0390022f419eda8ac499c23035d335a
Luke Taylor <luke@deentaylor.com>
2018-02-16 10:35:46 -0500

    Use feather-icons components in login modal

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,11 +1,11 @@
 <template>
   <form class="login-form" v-on:keydown.enter="login">
     <div>
-      <i data-feather="user"></i>
+      <user-icon></user-icon>
       <input type="text" v-model="username" placeholder="username" ref="usernameInput">
     </div>
     <div>
-      <i data-feather="lock"></i>
+      <lock-icon></lock-icon>
       <input type="password" v-model="password" placeholder="password" ref="passwordInput">
     </div>
     <a v-on:click="login">Sign in</a>
