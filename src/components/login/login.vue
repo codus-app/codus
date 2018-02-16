f129ef7a48b771e76936daa2312efc6013f93331
Luke Taylor <luke@deentaylor.com>
2018-02-16 10:26:33 -0500

    Feather icons in login page

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,11 +1,11 @@
 <template>
   <form class="login-form" v-on:keydown.enter="login">
     <div>
-      <i class="material-icons">person</i>
+      <i data-feather="user"></i>
       <input type="text" v-model="username" placeholder="username" ref="usernameInput">
     </div>
     <div>
-      <i class="material-icons">vpn_key</i>
+      <i data-feather="lock"></i>
       <input type="password" v-model="password" placeholder="password" ref="passwordInput">
     </div>
     <a v-on:click="login">Sign in</a>
