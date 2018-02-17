c36c8812e995c1df50142f6d1bb43834892cb266
Luke Taylor <luke@deentaylor.com>
2018-02-17 10:36:16 -0500

    Add 'collapsed' mode to sidebar

--- src/app/components/sidebar/sidebar.vue
+++ src/app/components/sidebar/sidebar.vue
@@ -1,23 +1,25 @@
 <template>
-  <div class="sidebar">
-    <h1>Codus</h1>
+  <div class="sidebar" v-bind:class="{ collapsed }">
+    <h1 v-if="!collapsed">Codus</h1>
 
     <div class="links">
 
       <!-- Personal user-related links -->
-      <h2>You</h2>
+      <h2 v-if="!collapsed">You</h2>
       <router-link class="sb-link" v-for="r in userRoutes" v-bind:key="r.path" v-bind:to="r.path">
-        <div class="indicator"></div>
+        <div class="indicator" v-if="!collapsed"></div>
         <component v-bind:is="`${r.meta.icon}-icon`"></component>
-        <span class="montserrat light">{{r.meta.label}}</span>
+        <span class="montserrat light" v-if="!collapsed">{{r.meta.label}}</span>
       </router-link>
 
+      <div class="divider" v-if="collapsed"></div> <!-- For spacing if sidebar is collapsed -->
+
       <!-- Class-related links -->
-      <h2>Class</h2>
+      <h2 v-if="!collapsed">Class</h2>
       <router-link class="sb-link" v-for="r in classRoutes" v-bind:key="r.path" v-bind:to="r.path">
-        <div class="indicator"></div>
+        <div class="indicator" v-if="!collapsed"></div>
         <component v-bind:is="`${r.meta.icon}-icon`"></component>
-        <span class="montserrat light">{{r.meta.label}}</span>
+        <span class="montserrat light" v-if="!collapsed">{{r.meta.label}}</span>
       </router-link>
 
     </div>
@@ -32,6 +34,8 @@ export default {
   data: () => ({
     userRoutes: routes.filter(r => r.meta.category === 'user'),
     classRoutes: routes.filter(r => r.meta.category === 'class'),
+
+    collapsed: false,
   }),
 };
 
