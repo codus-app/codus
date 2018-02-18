11649cce6a1748d5421bd1f2d9f3b643fa9460aa
Luke Taylor <luke@deentaylor.com>
2018-02-18 16:56:39 -0500

    Add module for consuming API

---
+++ src/app/api.js
@@ -0,0 +1,24 @@
+import path from 'path';
+
+const base = 'https://api.codus.arkis.io/';
+
+/** Most generic function */
+function apiRequest(endpoint, method, heads, body) {
+  const headers = { Authorization: `Bearer ${localStorage.access_token}` };
+  Object.assign(headers, heads);
+
+  return fetch(path.join(base, endpoint), {
+    headers, method, body,
+  });
+}
+
+/** Perform a GET request and return a promise */
+export function get(endpoint) {
+  return apiRequest(endpoint, 'GET');
+}
+
+/** Perform a PUT request with the given body */
+export function put(endpoint, body, contentType = 'text/plain') {
+  const headers = { 'Content-Type': contentType };
+  return apiRequest(endpoint, 'PUT', headers, body);
+}
