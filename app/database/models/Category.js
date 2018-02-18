66914452ec7ffe91ea81878437c0edd60034a4b4
Luke Taylor <luke@deentaylor.com>
2018-02-17 21:30:05 -0500

    add addProblem method to Category

--- app/database/models/Category.js
+++ app/database/models/Category.js
@@ -5,4 +5,15 @@ const categorySchema = new mongoose.Schema({
   problems: [String],
 });
 
+// Add a mongoose problem document to the category
+categorySchema.methods.addProblem = async function addProblem(problem) {
+  if (problem && !this.problems.includes(problem.name)) {
+    this.problems.push(problem.name); // Add to category's record
+    await this.save();
+    // Problem should have its category property set to this
+    problem.category = this.name();
+    await problem.save();
+  }
+};
+
 module.exports = mongoose.model('Category', categorySchema);
