002c9ae7a92b1dd8e10275f1abc95a10f7945bcb
Luke Taylor <luke@deentaylor.com>
2018-02-17 21:30:18 -0500

    add removeProblem method to Category

--- app/database/models/Category.js
+++ app/database/models/Category.js
@@ -15,5 +15,15 @@ categorySchema.methods.addProblem = async function addProblem(problem) {
     await problem.save();
   }
 };
+// Remove a problem from the category
+categorySchema.methods.removeProblem = async function removeProblem(problem) {
+  if (problem && this.problems.includes(problem.name)) {
+    this.problems.pull(problem.name); // Remove from category's record
+    await this.save();
+    // Problem should no longer reference this category
+    problem.category = undefined;
+    problem.save();
+  }
+};
 
 module.exports = mongoose.model('Category', categorySchema);
