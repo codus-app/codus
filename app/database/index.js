f9532ffe46f58963e2149b9c0d3f674a342acc73
Luke Taylor <luke@deentaylor.com>
2018-02-18 11:14:51 -0500

    Return promises instead of awaiting and returning result

--- app/database/index.js
+++ app/database/index.js
@@ -9,10 +9,9 @@ module.exports.getUser = {
   // Find a user with a given auth0 ID
   async byAuth0(sub) {
     await db.ready;
-    const user = await models.User
+    return models.User
       .findOne()
       .where('auth0_id').equals(sub);
-    return user;
   },
   byId(sub) { return this.byAuth0(sub); },
 };
@@ -20,28 +19,25 @@ module.exports.getUser = {
 // Find a problem in a given category with a given name
 module.exports.getProblem = async function getProblem(category, name) {
   await db.ready;
-  const problem = await models.Problem
+  return models.Problem
     .findOne()
     .where('category').equals(category)
     .where('name').equals(name);
-  return problem;
 };
 
 // Get the names of all categories
 module.exports.getCategories = async function getCategories() {
   await db.ready;
-  const categories = await models.Category
+  return models.Category
     .find()
     .select('name')
     .then(cats => cats.map(c => c.name)); // Return the name field of each object
-  return categories;
 };
 
 // Get a single Category by name
 module.exports.getCategory = async function getCategory(categoryName) {
   await db.ready;
-  const category = await models.Category
+  return models.Category
     .findOne()
     .where('name').equals(categoryName);
-  return category;
 };
