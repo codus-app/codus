d84dd687ae389fb854868918c635a85ae4c19492
Luke Taylor <luke@deentaylor.com>
2018-02-18 13:05:47 -0500

    Replace stripId with new strip flag in main server file

--- app/helpers.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // Remove _id and __v keys from Mongoose documents and arrays of mongoose documents
-  stripId(obj) {
-    if (Array.isArray(obj)) return obj.map(o => module.exports.stripId(o)); // Recur for arrays
-    const o = 'toObject' in obj ? obj.toObject() : obj; // Convert mongoose documents to JS objects
-    return Object.assign(o, { _id: undefined, __v: undefined }); // Remove properties
-  },
-};
--- app/index.js
+++ app/index.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 const auth0 = require('./auth');
 // Database stuff
 const database = require('./database');
-// Helper methods
-const { stripId } = require('./helpers');
 
 
 const app = express();
@@ -25,35 +23,33 @@ app.get('/', (req, res) => {
 
 // Query the database for a problem
 app.get('/problem/:category/:name', async (req, res) => {
-  const problem = await database.getProblem(req.params.category, req.params.name);
+  const problem = await database.getProblem(req.params.category, req.params.name, true);
   if (!problem) res.status(404).json({ error: `Problem ${req.params.name} was not found` });
-  else res.json(stripId(problem));
+  else res.json(problem);
 });
 
 // Get all the categories
 app.get('/categories', async (req, res) => {
-  database.getCategories()
-    .then(stripId)
+  database.getCategories(true)
     .then(cats => res.json(cats));
 });
 
 // Get all the problems in a category
 app.get('/category/:name', async (req, res) => {
-  const category = await database.getCategory(req.params.name);
+  const category = await database.getCategory(req.params.name, true);
   if (!category) res.status(404).json({ error: `Category ${req.params.name} was not found` });
   else {
-    const problems = await category.getProblems();
+    const problems = await category.getProblems(true);
     const out = category.toObject();
     out.problems = problems;
-    res.json(stripId(out));
+    res.json(out);
   }
 });
 
 
 // Query the database for the authenticated user and return all info
 app.get('/user', auth0(), (req, res) => {
-  database.getUser(req.user.sub)
-    .then(stripId) // Remove _id key
+  database.getUser(req.user.sub, true)
     .then(u => res.json(u));
 });
 
@@ -67,13 +63,13 @@ app.get('/userinfo', auth0(), (req, res) => {
 // of all contained problems, and whether the user's solution to each passes
 app.get('/categoryOverview/:category', auth0(), async (req, res) => {
   const user = await database.getUser(req.user.sub);
-  const category = await database.getCategory(req.params.category);
+  const category = await database.getCategory(req.params.category, true);
   if (!category) res.status(404).json({ error: `Category ${req.params.name} was not found` });
   else {
     const solutions = await category.getSolutions(user);
     const out = category.toObject();
     out.solutions = solutions;
-    res.json(stripId(out));
+    res.json(out);
   }
 });
 
@@ -82,7 +78,7 @@ app.get('/solution/:problemName', auth0(), async (req, res) => {
   const user = await database.getUser(req.user.sub);
   const solution = await user.getSolution(req.params.problemName);
   if (!solution) res.status(404).json({ error: `no solution by the authenticated user for problem ${req.params.problemName} was found` });
-  else res.json(stripId(solution));
+  else res.json(solution);
 });
 
 // Add/change a user's solution to a problem
