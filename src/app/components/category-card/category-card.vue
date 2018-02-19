294508e005d1df608eb956d60cc8a61b253b05f9
Luke Taylor <luke@deentaylor.com>
2018-02-19 08:48:22 -0500

    Add key to problem-link v-for

--- src/app/components/category-card/category-card.vue
+++ src/app/components/category-card/category-card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="category-card">
     <h1>{{ name }}</h1>
-    <problem-link v-for="prob in problems" v-bind:problem="prob.name" v-bind:completed="prob.passed">{{prob.name}}</problem-link>
+    <problem-link v-for="p in problems" v-bind:key="p.name" v-bind:problem="p.name" v-bind:completed="p.passed">{{p.name}}</problem-link>
   </div>
 </template>
 
