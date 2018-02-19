a3de690bbb77ed429d92817ddf51cf92e3eb6529
Luke Taylor <luke@deentaylor.com>
2018-02-19 11:29:32 -0500

    Compute which problems overflow a category-card and hide those

--- src/app/components/category-card/category-card.vue
+++ src/app/components/category-card/category-card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="category-card">
+  <div class="category-card" v-bind:class="{ invisible }">
     <h1>{{ name }}</h1>
-    <problem-link v-for="p in problems" v-bind:key="p.name" v-bind:problem="p.name" v-bind:completed="p.passed">{{p.name}}</problem-link>
+    <problem-link v-for="p in displayProblems" v-bind:key="p.name" v-bind:problem="p.name" v-bind:completed="p.passed" v-bind:ref="p.name">{{p.name}}</problem-link>
   </div>
 </template>
 
--- src/app/components/category-card/script.js
+++ src/app/components/category-card/script.js
@@ -5,7 +5,10 @@ export default {
   data: () => ({
     name: this.categoryId, // Will be replaced with readable name after first HTTP request completes
     description: '',
-    problems: [],
+
+    problems: [], // All problems in the category
+    displayProblems: [], // All problems that fit on the unexpanded card
+    invisible: true, // Should the problems be visible yet?
   }),
 
   created() {
@@ -23,6 +26,24 @@ export default {
       const solvedNames = category.solutions.filter(s => s.passed).map(s => s.name);
 
       problemNames.forEach(p => this.problems.push({ name: p, passed: solvedNames.includes(p) }));
+
+      this.displayProblems = this.problems;
+
+      // After items have rendered
+      setTimeout(() => {
+        // An array containing the Y position of each problem's link
+        const renderYs = this.problems.map(p => this.$refs[p.name][0].$el.offsetTop);
+        // Each unique problem Y position; the Y position of each row of links
+        const rowYs = renderYs.filter((y, i, arr) => arr.indexOf(y) === i);
+
+        // Need to hide some if there are more than 2 rwos
+        if (rowYs.length > 2) {
+          // Cut to the first two rows of problems
+          const first2Rows = this.displayProblems.filter((prob, i) => renderYs[i] < rowYs[2]);
+          this.displayProblems = first2Rows;
+        }
+        this.invisible = false;
+      });
     },
   },
 };
