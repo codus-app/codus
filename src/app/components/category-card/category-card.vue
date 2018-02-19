e46344ae450de6a77aff468ae837b1bb87131e6b
Luke Taylor <luke@deentaylor.com>
2018-02-19 12:00:23 -0500

    Add remainder indicator

--- src/app/components/category-card/category-card.vue
+++ src/app/components/category-card/category-card.vue
@@ -2,6 +2,7 @@
   <div class="category-card" v-bind:class="{ invisible }">
     <h1>{{ name }}</h1>
     <problem-link v-for="p in displayProblems" v-bind:key="p.name" v-bind:problem="p.name" v-bind:completed="p.passed" v-bind:ref="p.name">{{p.name}}</problem-link>
+    <div class="remainder" v-if="remainder">+{{remainder}}</div>
   </div>
 </template>
 
