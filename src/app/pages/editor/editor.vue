99a8a142ae0ac84f2bb5e2c640eea296dcd454d4
Luke Taylor <luke@deentaylor.com>
2018-02-20 10:52:13 -0500

    Move script and style out of main editor vue file

--- src/app/pages/editor/editor.vue
+++ src/app/pages/editor/editor.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="editor">
-    <span class="code bold">Category:&nbsp;</span><span class="code light">{{ $route.params.category }}</span>
+    <span class="code bold">Category:&nbsp;</span><span class="code light">
+      {{ $route.params.category }}
+    </span>
     <br/>
-    <span class="code bold">Problem:&nbsp;&nbsp;</span><span class="code light">{{ $route.params.name }}</span>
+    <span class="code bold">Problem:&nbsp;&nbsp;</span><span class="code light">
+      {{ $route.params.name }}
+    </span>
   </div>
 </template>
 
-<script> export default {}; </script>
-
-<style scoped lang="sass">
-.editor
-  padding: 2em
-  font-family: Montserrat
-
-.title
-  font-family:
-</style>
+<script src="./script.js"></script>
+<style scoped lang="sass" src="./style.sass"></style>
