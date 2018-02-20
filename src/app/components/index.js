e46c680635ef430cb406ccfe77a14188709b2cbb
Luke Taylor <luke@deentaylor.com>
2018-02-20 17:33:05 -0500

    Reorder app components

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,16 +1,12 @@
+/* eslint-disable import/first */
 import Vue from 'vue';
 
+// External components
 import { CodeMirror } from 'vue-codemirror';
-
-import { HomeIcon, BarChart2Icon, ClockIcon, InboxIcon, UsersIcon, AwardIcon, LogOutIcon } from 'vue-feather-icons';
-
-import Sidebar from './sidebar/sidebar.vue';
-import ProblemLink from './category-card/problem-link';
-import CategoryCard from './category-card/category-card.vue';
-import ProgressPie from './progress-pie/progress-pie.vue';
-
 Vue.component('codemirror', CodeMirror);
 
+// Feather icons
+import { HomeIcon, BarChart2Icon, ClockIcon, InboxIcon, UsersIcon, AwardIcon, LogOutIcon } from 'vue-feather-icons';
 Vue.component('home-icon', HomeIcon);
 Vue.component('bar-chart-2-icon', BarChart2Icon);
 Vue.component('clock-icon', ClockIcon);
@@ -19,6 +15,11 @@ Vue.component('users-icon', UsersIcon);
 Vue.component('award-icon', AwardIcon);
 Vue.component('log-out-icon', LogOutIcon);
 
+// Internal components
+import Sidebar from './sidebar/sidebar.vue';
+import ProblemLink from './category-card/problem-link';
+import CategoryCard from './category-card/category-card.vue';
+import ProgressPie from './progress-pie/progress-pie.vue';
 Vue.component('sidebar', Sidebar);
 Vue.component('problem-link', ProblemLink);
 Vue.component('category-card', CategoryCard);
