b3bb339be86f5398332cf459a8c57beab63b262e
Luke Taylor <luke@deentaylor.com>
2018-02-19 23:40:12 -0500

    progress as prop instead of data
    should be used with v-bind:progress in order to dynamically modify

--- src/app/components/progress-pie/script.js
+++ src/app/components/progress-pie/script.js
@@ -3,14 +3,7 @@ export default {
     colors: Array,
     background: String,
     gradientAngle: { type: Number, default: 135 },
-    value: { type: Number, default: 0 },
-  },
-
-  data() {
-    return {
-      progress: this.value,
-      gradAngle: this.gradientAngle,
-    };
+    progress: { type: Number, default: 0 },
   },
 
   methods: {
@@ -18,13 +11,13 @@ export default {
       const progressAngle = this.progress * 360;
       if (progressAngle <= 180) { // Only right side needs to be adjusted
         this.$refs.rightFill.style.transform = `rotate(${progressAngle}deg)`;
-        this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradAngle - progressAngle}deg, ${this.colors[0]}, ${this.colors[1]})`;
+        this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - progressAngle}deg, ${this.colors[0]}, ${this.colors[1]})`;
         this.$refs.leftFill.style.transform = 'rotate(0deg)';
       } else {
         this.$refs.rightFill.style.transform = 'rotate(180deg)';
-        this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradAngle - 180}deg, ${this.colors[0]}, ${this.colors[1]})`;
+        this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - 180}deg, ${this.colors[0]}, ${this.colors[1]})`;
         this.$refs.leftFill.style.transform = `rotate(${progressAngle - 180}deg)`;
-        this.$refs.leftFill.style.backgroundImage = `linear-gradient(${this.gradAngle - (progressAngle - 180)}deg, ${this.colors[0]}, ${this.colors[1]})`;
+        this.$refs.leftFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - (progressAngle - 180)}deg, ${this.colors[0]}, ${this.colors[1]})`;
       }
     },
   },
