61870c309badc7f3db036b8d61ba6a4c748e5bb9
Luke Taylor <luke@deentaylor.com>
2018-02-20 23:51:14 -0500

    Add matchbrackets to codemirror

--- src/app/pages/editor/codemirror-config.js
+++ src/app/pages/editor/codemirror-config.js
@@ -2,10 +2,13 @@
 import 'codemirror/mode/clike/clike';
 // Theme
 import 'codemirror/theme/base16-dark.css';
+// Addons
+import 'codemirror/addon/edit/matchbrackets';
 
 export default {
   tabSize: 2,
   mode: 'text/x-java',
   theme: 'base16-dark',
   lineNumbers: true,
+  matchBrackets: true,
 };
