bb0969e4bf91383e977913ce3b0d8467c5dd660e
Luke Taylor <luke@deentaylor.com>
2018-02-21 23:42:49 -0500

    Enable jackhammer theme

--- src/app/pages/editor/codemirror-config.js
+++ src/app/pages/editor/codemirror-config.js
@@ -1,17 +1,19 @@
 // Mode
 import 'codemirror/mode/clike/clike';
-// Theme
+// Preinstalled themes
 import 'codemirror/theme/material.css';
 // Addons
 import 'codemirror/addon/edit/matchbrackets';
 import 'codemirror/addon/edit/closebrackets';
 // Additional styles
 import './codemirror.sass';
+// Custom themes
+import './themes/jackhammer.sass';
 
 export default {
   tabSize: 2,
   mode: 'text/x-java',
-  theme: 'material',
+  theme: 'jackhammer',
   lineNumbers: true,
   matchBrackets: true,
   autoCloseBrackets: true,
