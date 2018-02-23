98f868c069e1f4e1fa95fb352dbedffefa4ff159
Luke Taylor <luke@deentaylor.com>
2018-02-22 21:52:21 -0500

    Add square in top left editor corner in order to fill in gap

--- src/app/pages/editor/editor.vue
+++ src/app/pages/editor/editor.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="editor">
+    <div class="corner-square"></div>
+
     <div class="top-bar"></div>
 
     <codemirror v-model="code" v-bind:options="cmOptions"></codemirror>
@@ -10,10 +12,20 @@
 <script src="./script.js"></script>
 <style scoped lang="sass" src="./style.sass"></style>
 
+<!-- This can't be scoped so it gets its own style tag -->
 <style lang="sass">
   @import style
 
   .sidebar.collapsed
     top: $bar-width + $gap
     height: calc(100vh - #{$bar-width + $gap})
+
+    &::before
+      content: ''
+      background: $dark-1
+      position: absolute
+      top: -#{$bar-width + $gap}
+      left: 0
+      width: $bar-width
+      height: $bar-width
 </style>
