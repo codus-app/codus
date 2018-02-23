c26e4f559f2f1b469b7f4c51367b9e58f46dc7d1
Luke Taylor <luke@deentaylor.com>
2018-02-22 21:38:41 -0500

    Shift sidebar down in editor

--- src/app/pages/editor/editor.vue
+++ src/app/pages/editor/editor.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="editor">
-    <div class="top-bar">
-      <div class="title">
-        <span class="montserrat medium">{{ category }} /</span>
-        <span class="montserrat extralight">{{ problemName }}</span>
-      </div>
-    </div>
+    <div class="top-bar"></div>
 
     <codemirror v-model="code" v-bind:options="cmOptions"></codemirror>
 
@@ -14,3 +9,11 @@
 
 <script src="./script.js"></script>
 <style scoped lang="sass" src="./style.sass"></style>
+
+<style lang="sass">
+  @import style
+
+  .sidebar.collapsed
+    top: $bar-width + $gap
+    height: calc(100vh - #{$bar-width + $gap})
+</style>
