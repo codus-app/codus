0e53b1602dd0530f9f56c59162da330895a4d015
Luke Taylor <luke@deentaylor.com>
2018-08-11 16:10:23 -0400

    use router-link instead of a tag in problem-link

--- src/app/components/category-card/problem-link.js
+++ src/app/components/category-card/problem-link.js
@@ -1,9 +1,14 @@
 export default {
-  template: '<a v-bind:href="location" class="problem-link" v-bind:class="{passed}">{{ name }}</a>',
+  template: '<router-link v-bind:to="location" class="problem-link" v-bind:class="{passed}">{{ name }}</router-link>',
 
   props: ['category', 'name', 'passed'],
 
   computed: {
-    location() { return `/app/problem/${this.category}/${this.name}`; },
+    location() {
+      return {
+        name: 'problem',
+        params: { category: this.category, name: this.name },
+      };
+    },
   },
 };
