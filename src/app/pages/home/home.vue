3545878f03744c6346dd41c0ae17b4ca9d10cdd5
Luke Taylor <luke@deentaylor.com>
2018-08-14 09:20:41 -0400

    Add fade at the bottom of categories scroll container

--- src/app/pages/home/home.vue
+++ src/app/pages/home/home.vue
@@ -3,12 +3,10 @@
     <div class="cards">
       <category-card v-for="id in categories" v-bind:key="id" v-bind:category-id="id"></category-card>
     </div>
+    <div class="fade"></div>
   </div>
 </template>
 
 <script src="./script.js"></script>
 
-<style scoped lang="sass">
-.page
-  padding: 1.2rem
-</style>
+<style scoped lang="sass" src="./style.sass"></style>
