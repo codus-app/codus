3c95ab72da532ff6cd186f902bf2f8574f0f1d80
Luke Taylor <luke@deentaylor.com>
2018-09-05 21:15:41 -0400

    Add javaStringToJS function for safe eval of simple supported Java types to JS values

--- app/java.js
+++ app/java.js
@@ -45,3 +45,40 @@ module.exports.isValidIdentifier = function isValidIdentifier(obj) {
   // Sticking to the enligh alphabet because there's no easy way to do it properly
   return /^[a-zA-Z_][a-zA-Z0-9_]*$/.test(obj);
 };
+
+module.exports.javaStringToJS = function convertStringWithType(valueString, javaType) {
+  const failureMessage = `Could not convert value "${valueString}" to ${javaType}`;
+  if (!module.exports.types.includes(javaType)) throw new Error(`javaType must be one of ${module.exports.types.join(', ')}`);
+  else {
+    let valueString2 = valueString.trim();
+    const jsType = module.exports.toJs[javaType];
+    // Array: split array into each element, recur for each
+    if (Array.isArray(jsType)) {
+      if (!valueString2.startsWith('[') || !valueString2.endsWith(']')) throw new Error('For array conversions, valueString must be a properly formatted Java array (starting with [ and ending with ])');
+      const elements = valueString2.slice(1, -1).split(',').map(val => val.trim()).filter(val => val.length);
+      return elements.map(el => module.exports.javaStringToJS(el, javaType.slice(0, -2)));
+    }
+    // String: Use JSON.parse to safely get string value
+    if (jsType === String) {
+      if (valueString2.startsWith("'") && valueString2.endsWith("'")) {
+        // If the string is single-quoted, escape all double quotes and replace the beginning/end
+        // with double quotes
+        valueString2 = `"${valueString2.slice(1, -1).replace(/"/g, '\\"')}"`;
+      }
+      // Use JSON.parse for easy safe string eval
+      return JSON.parse(valueString2);
+    }
+    // Number: Parse with Number constructor
+    if (jsType === Number) {
+      const out = Number(valueString2);
+      if (Number.isNaN(out)) throw new Error(failureMessage);
+      return out;
+    }
+    // Boolean: simple parse logic
+    if (jsType === Boolean) {
+      if (!['true', 'false'].includes(valueString2)) throw new Error(failureMessage);
+      return { 'true': true, 'false': false }[valueString2];
+    }
+    throw new Error('Something went wrong');
+  }
+};
