1f209f2e520212c7b17550f914477e92ed9931d4
Luke Taylor <luke@deentaylor.com>
2018-09-13 23:55:43 -0400

    Add categories list API endpoint

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,5 +1,17 @@
 /* eslint-disable global-require */
 
+const keystone = require('keystone');
+
+const Category = keystone.list('Category');
 module.exports = {
   base(req, res) { res.json({ status: 'ok' }); },
+
+  category: {
+    async list(req, res) {
+      Category.model
+        .find()
+        .select('-_id -__v')
+        .then(cats => res.json(cats));
+    },
+  },
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -8,4 +8,6 @@ module.exports = (app) => {
   app.get('/', (req, res) => res.send('ok'));
 
   app.get('/api', routes.api.base);
+
+  app.get('/api/categories', routes.api.category.list);
 };
