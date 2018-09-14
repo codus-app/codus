72f4c6ae55839943955e4aa93f9251b899a02f49
Luke Taylor <luke@deentaylor.com>
2018-09-13 23:53:29 -0400

    Begin scaffolding API routes

---
+++ app/routes/api/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable global-require */
+
+module.exports = {
+  base(req, res) { res.json({ status: 'ok' }); },
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,11 @@
+/* eslint-disable global-require */
+const routes = {
+  api: require('./api'),
+};
+/* eslint-enable global-require */
+
 module.exports = (app) => {
   app.get('/', (req, res) => res.send('ok'));
+
+  app.get('/api', routes.api.base);
 };
