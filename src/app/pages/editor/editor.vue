e16b107b6efc21989ef6e0dbc11283a70f1e629a
Luke Taylor <luke@deentaylor.com>
2018-10-21 14:58:45 -0400

    New style with rounded corners on editor page

--- src/app/pages/editor/editor.vue
+++ src/app/pages/editor/editor.vue
@@ -2,7 +2,6 @@
   <div class="editor-page">
     <!-- Problem browser -->
     <div class="problem-browser">
-      <div class="top-bar">Problems</div>
       <problems-tree class="listing"></problems-tree>
     </div>
 
@@ -30,21 +29,3 @@
 
 <script src="./script.js"></script>
 <style scoped lang="sass" src="./style.sass"></style>
-
-<!-- This can't be scoped so it gets its own style tag -->
-<style lang="sass">
-  @import style
-
-  .sidebar.collapsed
-    top: $bar-width + $gap
-    height: calc(100vh - #{$bar-width + $gap})
-
-    &::before
-      content: ''
-      background: $dark-1
-      position: absolute
-      top: -#{$bar-width + $gap}
-      left: 0
-      width: $bar-width
-      height: $bar-width
-</style>
