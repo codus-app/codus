0fc8a9a62c749258af97cddf01d3846017720770
Luke Taylor <luke@deentaylor.com>
2018-10-23 22:54:06 -0400

    Add full text to test-result-card

--- src/app/components/test-result-card/test-result-card.vue
+++ src/app/components/test-result-card/test-result-card.vue
@@ -1,6 +1,29 @@
 <template>
+  <div class="test-result-card code">
     <div class="status-indicator" v-bind:class="{ green: passed, red: passed === false }"></div>
 
+    <div class="collapsed">
+      main({{ parameters.map(p => JSON.stringify(p)).join(', ') }})
+      ->
+      {{ JSON.stringify(expectedResult) }}
+    </div>
+
+    <div class="expanded">
+      <p>
+        <span>Function call:</span>
+        main({{ parameters.map(p => JSON.stringify(p)).join(', ') }})
+        <br/>
+        <span>Expected result:</span>
+        {{ JSON.stringify(expectedResult) }}
+      </p>
+
+      <p>
+        <span>Your solution returned:</span>
+        {{ JSON.stringify(result) }}
+        <br/>
+        <span>Pass:</span> {{ passed }}
+      </p>
+    </div>
   </div>
 </template>
 
