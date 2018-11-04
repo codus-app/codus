abd49ec60b74dd5bc2513d34e0eee8f75cb65fe7
Luke Taylor <luke@deentaylor.com>
2018-11-04 16:30:28 -0500

    Try to resolve a few of the progress-pie rendering issues

--- src/app/components/progress-pie/script.js
+++ src/app/components/progress-pie/script.js
@@ -10,13 +10,13 @@ export default {
     update() {
       const progressAngle = this.progress * 360;
       if (progressAngle <= 180) { // Only right side needs to be adjusted
-        this.$refs.rightFill.style.transform = `rotate(${progressAngle}deg)`;
+        this.$refs.rightFill.style.transform = `scale(1.1) rotate(${progressAngle}deg)`;
         this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - progressAngle}deg, ${this.colors[0]}, ${this.colors[1]})`;
-        this.$refs.leftFill.style.transform = 'rotate(0deg)';
+        this.$refs.leftFill.style.transform = 'scale(1.1) rotate(0deg)';
       } else {
-        this.$refs.rightFill.style.transform = 'rotate(180deg)';
+        this.$refs.rightFill.style.transform = 'scale(1.1) rotate(180deg)';
         this.$refs.rightFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - 180}deg, ${this.colors[0]}, ${this.colors[1]})`;
-        this.$refs.leftFill.style.transform = `rotate(${progressAngle - 180}deg)`;
+        this.$refs.leftFill.style.transform = ` scale(1.1) rotate(${progressAngle - 180}deg)`;
         this.$refs.leftFill.style.backgroundImage = `linear-gradient(${this.gradientAngle - (progressAngle - 180)}deg, ${this.colors[0]}, ${this.colors[1]})`;
       }
     },
