94467cb5a38aba6ea530eb0d9bb3da6baafa8c4a
Luke Taylor <luke@deentaylor.com>
2018-11-04 21:19:56 -0500

    Add hidden test cases to Problem model

--- app/models/Problem.js
+++ app/models/Problem.js
@@ -50,6 +50,18 @@ Problem.add({
       message: 'Test case must contain both parameters and expected result, separated by "->"',
     },
   },
+
+  // Each test case should take the format "arg1, arg2, arg3 -> expectedResult"
+  hiddenTestCases: {
+    type: Types.TextArray,
+    note: 'Formatted as arg1, arg2, arg3... -> expectedResult',
+    initial: true,
+    required: false,
+    validate: {
+      validator: arr => arr.every(val => val.includes('->')),
+      message: 'Test case must contain both parameters and expected result, separated by "->"',
+    },
+  },
 });
 
 Problem.schema.virtual('parameters2').get(function expandedParams() {
@@ -62,18 +74,22 @@ Problem.schema.virtual('testCases2').get(function expandedTestCases() {
   const functionParams = this.parameters.map(p => p.split('|').map(s => s.trim()));
   const paramTypes = functionParams.map(p => p[1]);
 
-  return this.testCases
+  return this.testCases.concat(this.hiddenTestCases)
     // Split parameters out from expected result
     .map(t => t.split('->').map(part => part.trim()))
-    .map(([parameters, expectedResult]) => ({
+    .map(([parameters, expectedResult], i) => ({
       parameters: parameters
         // Split up parameters
         .split(',').map(param => param.trim()) // split out each parameter
         // Convert each parameter to the correct JS type
-        .map((param, i) => java.javaStringToJS(param, paramTypes[i])),
+        .map((param, i2) => java.javaStringToJS(param, paramTypes[i2])),
 
       // Convert result to the correct JS type
       result: java.javaStringToJS(expectedResult, this.resultType),
+
+      // If the index of the past the length of the public tests, it came from the hiddenTestCases
+      // array that was concatted on
+      hidden: i >= this.testCases.length,
     }));
 });
 
@@ -81,3 +97,4 @@ Problem.schema.virtual('testCases2').get(function expandedTestCases() {
 Problem.register();
 
 Problem.defaultColumns = 'name, category, resultType';
+Problem.defaultSort = 'category';
