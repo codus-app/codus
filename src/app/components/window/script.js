6cec50936564fb449bc5fc1d5ebb940f2820cb66
Luke Taylor <luke@deentaylor.com>
2018-11-13 22:44:11 -0500

    Don't allow dragging window outside of bounds

--- src/app/components/window/script.js
+++ src/app/components/window/script.js
@@ -1,3 +1,5 @@
+import { clip } from '../../helpers';
+
 export default {
   props: {
     canResize: { type: Boolean, default: true },
@@ -49,9 +51,13 @@ export default {
       e.preventDefault();
       const [startX, startY] = this.mouseStart;
       const [prevX, prevY] = this.previousPos;
+      const minX = this.bounds[0] - this.initialBounds.left;
+      const maxX = this.bounds[2] - this.initialBounds.right;
+      const minY = this.bounds[1] - this.initialBounds.top;
+      const maxY = this.bounds[3] - this.initialBounds.bottom;
       this.pos = [
-        prevX + (e.clientX - startX),
-        prevY + (e.clientY - startY),
+        clip(prevX + (e.clientX - startX), minX, maxX),
+        clip(prevY + (e.clientY - startY), minY, maxY),
       ];
     },
 
