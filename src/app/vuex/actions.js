f360413bd4d4f8fbe4e16a2fe2526800e3f7382c
Luke Taylor <luke@deentaylor.com>
2018-11-15 10:22:10 -0500

    Update actions for new format

--- src/app/vuex/actions.js
+++ src/app/vuex/actions.js
@@ -3,20 +3,20 @@ import * as api from '../api';
 export default {
   // Update list of solutions
   async fetchSolved({ commit }) {
-    const solutions = await api.get('user/solutions');
+    const solutions = await api.get({ endpoint: 'user/solutions' });
     commit('updateSolvedList', solutions);
   },
 
   // Update basic info about category listings
   async fetchCategories({ commit }) {
-    const categories = await api.get('categories');
+    const categories = await api.get({ endpoint: 'categories' });
     commit('categoriesFetched', categories);
   },
 
 
   // Populate problem and solution info for a given problem
   async fetchSolution({ commit }, { category, problem }) {
-    const solution = await api.get(`user/solution/${category}/${problem}`);
+    const solution = await api.get({ endpoint: `user/solution/${category}/${problem}` });
     // Update problem info for the problem this solution came from
     const { problem: problemFetched } = solution;
     commit('problemFetched', problemFetched);
@@ -29,7 +29,10 @@ export default {
   // Save a solution to a problem
   async saveSolution({ commit }, { problem, category, code }) {
     commit('beginSolutionSave');
-    const { passed } = await api.put(`user/solution/${category}/${problem}`, { code });
+    const { passed } = await api.put({
+      endpoint: `user/solution/${category}/${problem}`,
+      body: { code },
+    });
     commit('updateSolution', { problem, category, code });
     commit('updateSolved', { problem, category, passed });
     commit('endSolutionSave');
@@ -37,7 +40,11 @@ export default {
 
   async checkSolution({ commit }, { problem, category }) {
     commit('beginSolutionCheck');
-    const { tests, passed, error, solution } = await api.get(`user/solution/check/${category}/${problem}`); // eslint-disable-line object-curly-newline
+    const {
+      tests, passed, error, solution,
+    } = await api.get({
+      endpoint: `user/solution/check/${category}/${problem}`,
+    });
     commit('updateSolved', { problem, category, passed });
     commit('updateTestResults', { problem, category, tests, error, code: solution.code }); // eslint-disable-line object-curly-newline
     commit('endSolutionCheck');
