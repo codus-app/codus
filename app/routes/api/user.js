3d69bff5f206d358cd8b463eb44fa22509a0635f
Luke Taylor <luke@deentaylor.com>
2018-11-19 15:13:23 -0500

    Clean up 'user' endpoint response

--- app/routes/api/user.js
+++ app/routes/api/user.js
@@ -3,8 +3,14 @@ const { getUser: getAuth0User } = require('../auth');
 module.exports = {
   user: {
     async get(req, res) {
-      const userInfo = await getAuth0User(req.user.sub);
-      res.send(userInfo);
+      const { username, user_metadata, email, picture } = await getAuth0User(req.user.sub); // eslint-disable-line object-curly-newline, max-len, camelcase
+      res.send({
+        id: req.user.sub,
+        username,
+        name: user_metadata.name,
+        email,
+        picture,
+      });
     },
   },
 };
