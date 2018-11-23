133f31cb6556c833609df0434e57090664d78f13
Luke Taylor <luke@deentaylor.com>
2018-11-23 10:36:07 -0500

    Method for API PUT requests

--- src/app/api.js
+++ src/app/api.js
@@ -1,9 +1,10 @@
+/* eslint-disable object-curly-newline */
 const base = !Number.isNaN(parseInt(window.location.hostname, 10)) || window.location.hostname === 'localhost'
   ? `http://${window.location.hostname}:3000/api`
   : 'https://api.codus.arkis.io/api';
 
 /** Most generic function */
-async function apiRequest({ endpoint, method, heads, body, store }) { // eslint-disable-line object-curly-newline, max-len
+async function apiRequest({ endpoint, method, heads, body, store }) {
   // Renew if necessary before making an authenticated API call
   if (!store.getters['auth/loginValid']()) await store.dispatch('auth/renew');
 
@@ -26,15 +27,28 @@ export function get({ endpoint, store }) {
   return apiRequest({ endpoint, method: 'GET', store });
 }
 
-/** Perform a PUT request with the given body */
-export function put({ endpoint, body, contentType = 'application/json', store }) { // eslint-disable-line object-curly-newline, max-len
+/**
+ * Perform an HTTP request for a method that requires a body like GET or POST, implementing easy
+ * JSON support etc
+ */
+function requestWithBody({ endpoint, body, contentType = 'application/json', method, store }) {
   const heads = { 'Content-Type': contentType };
   const json = contentType === 'application/json';
   return apiRequest({
     endpoint,
-    method: 'PUT',
+    method,
     heads,
     body: json ? JSON.stringify(body) : body,
     store,
   });
+}
+
+/** Perform a PUT request with the given body */
+export function put({ endpoint, body, contentType, store }) {
+  return requestWithBody({ endpoint, method: 'PUT', body, contentType, store });
+}
+
+/** Perform a PATCH request with the given body */
+export function patch({ endpoint, body, contentType, store }) {
+  return requestWithBody({ endpoint, method: 'PATCH', body, contentType, store });
 }
