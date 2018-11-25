6284fa0dd45fd4905cc06c029a7206534a5ddd00
Luke Taylor <luke@deentaylor.com>
2018-11-24 21:00:16 -0500

    Track when account edit profile fields are changed

--- src/app/pages/settings/account/script.js
+++ src/app/pages/settings/account/script.js
@@ -8,6 +8,12 @@ export default {
 
   computed: {
     ...mapState({ profile: state => state.user.profile }),
+
+    profileChanged() {
+      const remote = this.profile;
+      return Object.keys(this.profile).length // Profile loaded
+        && (this.username !== remote.username || this.name !== remote.name); // Info changed
+    },
   },
 
   watch: {
