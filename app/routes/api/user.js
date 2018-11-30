af9f14c3d3b00963c274430721143a641415b880
Luke Taylor <luke@deentaylor.com>
2018-11-30 11:57:13 -0500

    Move validateUser method to util file

--- app/routes/api/user.js
+++ app/routes/api/user.js
@@ -1,38 +1,8 @@
-const { isEmail, isByteLength } = require('validator');
+const { validateUser } = require('./util');
 const {
   getUser: getAuth0User,
   updateUser: updateAuth0User,
 } = require('../auth');
-
-
-function validateUser({ username, name, email, password }, required = []) { // eslint-disable-line object-curly-newline, max-len
-  const errors = [];
-  // Username
-  if (required.includes('username') && !username) {
-    errors.push({ key: 'username', message: 'Username is required' });
-  } if (typeof username === 'string' && !isByteLength(username, { min: 1, max: 15 })) {
-    errors.push({ key: 'username', message: 'Must be between 1 and 15 characters' });
-  } if (typeof username === 'string' && username.match(/[^a-z0-9_]/)) {
-    errors.push({ key: 'username', message: 'Must only contain lowercase letters, numbers, and underscores' });
-  // Name
-  } if (required.includes('name') && !name) {
-    errors.push({ key: 'name', message: 'Name is required' });
-  } if (typeof name === 'string' && !isByteLength(name, { min: 1, max: 25 })) {
-    errors.push({ key: 'name', message: 'Must be between 1 and 25 characters' });
-  // Email
-  } if (required.includes('email') && !email) {
-    errors.push({ key: 'email', message: 'Email is required' });
-  } if (typeof email === 'string' && !isEmail(email)) {
-    errors.push({ key: 'email', message: 'Must be a valid email' });
-  // Password
-  } if (required.includes('password')) {
-    errors.push({ key: 'password', message: 'Password is required' });
-  } if (typeof password === 'string' && !isByteLength(name, { min: 8 })) {
-    errors.push({ key: 'password', message: 'Must be at least 8 characters in length' });
-  }
-  return errors;
-}
-
 
 /* eslint-disable camelcase */
 
--- app/routes/api/util.js
+++ app/routes/api/util.js
@@ -1,3 +1,5 @@
+const { isEmail, isByteLength } = require('validator');
+
 module.exports = {
   // Prepare a problem from mongoose to be exposed to the public.
   // Expands useful information, and hides secret information
@@ -24,5 +26,47 @@ module.exports = {
     _id: undefined,
     __v: undefined,
   }),
+
+
+  /**
+   * Validate a potential user:
+   *  • Username
+   *  • Name
+   *  • Email
+   *  • Password
+   * for length and other requirements.
+   */
+  validateUser({ username, name, email, password }, required = []) { // eslint-disable-line object-curly-newline, max-len
+    const errors = [];
+
+    // Username
+    if (required.includes('username') && !username) {
+      errors.push({ key: 'username', message: 'Username is required' });
+    } if (typeof username === 'string' && !isByteLength(username, { min: 1, max: 15 })) {
+      errors.push({ key: 'username', message: 'Must be between 1 and 15 characters' });
+    } if (typeof username === 'string' && username.match(/[^a-z0-9_]/)) {
+      errors.push({ key: 'username', message: 'Must only contain lowercase letters, numbers, and underscores' });
+
+    // Name
+    } if (required.includes('name') && !name) {
+      errors.push({ key: 'name', message: 'Name is required' });
+    } if (typeof name === 'string' && !isByteLength(name, { min: 1, max: 25 })) {
+      errors.push({ key: 'name', message: 'Must be between 1 and 25 characters' });
+
+    // Email
+    } if (required.includes('email') && !email) {
+      errors.push({ key: 'email', message: 'Email is required' });
+    } if (typeof email === 'string' && !isEmail(email)) {
+      errors.push({ key: 'email', message: 'Must be a valid email' });
+
+    // Password
+    } if (required.includes('password')) {
+      errors.push({ key: 'password', message: 'Password is required' });
+    } if (typeof password === 'string' && !isByteLength(name, { min: 8 })) {
+      errors.push({ key: 'password', message: 'Must be at least 8 characters in length' });
+    }
+
+    return errors;
+  },
 
 };
