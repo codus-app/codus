989784fa0e3b68e44f75fd7fc592a6318c972f0a
Luke Taylor <luke@deentaylor.com>
2018-12-04 00:52:31 -0500

    Remove extraneous sidebar links ig

--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -6,7 +6,7 @@
     <div class="links">
 
       <!-- Personal user-related links -->
-      <h2 v-if="!collapsed">You</h2>
+      <!-- <h2 v-if="!collapsed">You</h2> -->
       <router-link class="sb-link" v-for="r in userRoutes" v-bind:key="r.path" v-bind:to="r.path" v-bind:title="collapsed ? r.meta.label : ''">
         <div class="indicator" v-if="!collapsed"></div>
         <component v-bind:is="`icon-${r.meta.icon}`"></component>
@@ -14,14 +14,6 @@
       </router-link>
 
       <div class="divider" v-if="collapsed"></div> <!-- For spacing if sidebar is collapsed -->
-
-      <!-- Class-related links -->
-      <h2 v-if="!collapsed">Class</h2>
-      <router-link class="sb-link" v-for="r in classRoutes" v-bind:key="r.path" v-bind:to="r.path" v-bind:title="collapsed ? r.meta.label : ''">
-        <div class="indicator" v-if="!collapsed"></div>
-        <component v-bind:is="`icon-${r.meta.icon}`"></component>
-        <span class="font-1 regular" v-if="!collapsed">{{r.meta.label}}</span>
-      </router-link>
 
       <div class="sb-link logout" v-on:click="logout" v-bind:title="collapsed ? 'Log out' : ''">
         <icon-log-out></icon-log-out>
@@ -42,7 +34,6 @@ export default {
 
   data: () => ({
     userRoutes: routes.filter(r => r.meta.category === 'user'),
-    classRoutes: routes.filter(r => r.meta.category === 'class'),
   }),
 
   methods: { ...mapActions({ logout: 'auth/logout' }) },
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,39 +7,39 @@ export default [
     component: require('./home/home.vue').default,
     meta: { icon: 'home', label: 'Home', category: 'user' },
   },
-  {
-    path: '/statistics',
-    name: 'statitstics',
-    component: require('./statistics/statistics.vue').default,
-    meta: { icon: 'bar-chart', label: 'Statistics', category: 'user' },
-  },
-  {
-    path: '/recently-completed',
-    name: 'recent',
-    component: require('./recently-completed/recently-completed.vue').default,
-    meta: { icon: 'clock', label: 'Recently completed', category: 'user' },
-  },
+  // {
+  //   path: '/statistics',
+  //   name: 'statitstics',
+  //   component: require('./statistics/statistics.vue').default,
+  //   meta: { icon: 'bar-chart', label: 'Statistics', category: 'user' },
+  // },
+  // {
+  //   path: '/recently-completed',
+  //   name: 'recent',
+  //   component: require('./recently-completed/recently-completed.vue').default,
+  //   meta: { icon: 'clock', label: 'Recently completed', category: 'user' },
+  // },
 
   // Class
 
-  {
-    path: '/assignments',
-    name: 'assignments',
-    component: require('./assignments/assignments.vue').default,
-    meta: { icon: 'inbox', label: 'Assignments', category: 'class' },
-  },
-  {
-    path: '/students',
-    name: 'students',
-    component: require('./students/students.vue').default,
-    meta: { icon: 'users', label: 'Students', category: 'class' },
-  },
-  {
-    path: '/leaderboard',
-    name: 'leaderboard',
-    component: require('./leaderboard/leaderboard.vue').default,
-    meta: { icon: 'award', label: 'Leaderboard', category: 'class' },
-  },
+  // {
+  //   path: '/assignments',
+  //   name: 'assignments',
+  //   component: require('./assignments/assignments.vue').default,
+  //   meta: { icon: 'inbox', label: 'Assignments', category: 'class' },
+  // },
+  // {
+  //   path: '/students',
+  //   name: 'students',
+  //   component: require('./students/students.vue').default,
+  //   meta: { icon: 'users', label: 'Students', category: 'class' },
+  // },
+  // {
+  //   path: '/leaderboard',
+  //   name: 'leaderboard',
+  //   component: require('./leaderboard/leaderboard.vue').default,
+  //   meta: { icon: 'award', label: 'Leaderboard', category: 'class' },
+  // },
 
   // Problem editor
   // Won't render in sidebar because it has no category set in meta
