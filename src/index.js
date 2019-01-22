4846e165781d3ca6753596485b0d3da6b04975da
Luke Taylor <luke@deentaylor.com>
2019-01-21 21:09:54 -0500

    Don't make any API requests before login completes
    This also prevents erroneously calling "renew" right after login

--- src/index.js
+++ src/index.js
@@ -61,18 +61,16 @@ window.app = new Vue({
   },
 
   created() {
-    this.fetchUserProfile();
-
     // Promise for fetching some basic user data, which components can await
     this.fetchPromise = (async () => {
       // If we haven't fetched user data
       if (this.user.solved === null) {
         // We're already logged in
-        if (this.authValid()) await this.fetchSolved();
+        if (this.authValid()) await Promise.all([this.fetchUserProfile(), this.fetchSolved()]);
         // We're not logged in; wait for authentication (and *then* the request) to complete before
         // resolving
-        // "Once logged in, fetchSolved then resolve" hooray for semantic code
-        else await new Promise(resolve => this.$once('loggedIn', () => this.fetchSolved().then(resolve)));
+        // "Once logged in, fetchUserProfile, fetchSolved then resolve" hooray for semantic code
+        else await new Promise(resolve => this.$once('loggedIn', () => Promise.all([this.fetchUserProfile(), this.fetchSolved()]).then(resolve)));
       }
     })();
   },
