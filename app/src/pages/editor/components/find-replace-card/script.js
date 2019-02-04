087db4a942df19851974d8bffb24f3ce86342140
Luke Taylor <luke@deentaylor.com>
2019-02-04 17:40:18 -0500

    Search overlay

--- app/src/pages/editor/components/find-replace-card/script.js
+++ app/src/pages/editor/components/find-replace-card/script.js
@@ -9,6 +9,7 @@ export default {
     cursor: null,
     activeSearch: '',
     hasMatches: false,
+    overlay: null,
   }),
 
   computed: {
@@ -17,12 +18,33 @@ export default {
 
   methods: {
     setSearch() {
+      // Set up search cursor
       this.cursor = this.codemirror.getSearchCursor(this.query);
       this.activeSearch = this.query;
+      // Now stop if there are no results
+      if (!this.query.length) { this.hasMatches = false; return; }
+      // Check whether the search appears in the code
       this.cursor.findNext();
       this.hasMatches = this.cursor.atOccurrence;
       this.cursor.findPrevious();
     },
+
+    setOverlay() {
+      // Initialize search highlight
+      this.overlay = {
+        token: (stream) => {
+          if (stream.string.substring(stream.pos, stream.pos + this.query.length) === this.query) {
+            stream.pos += this.query.length;
+            return 'searching';
+          }
+          if (stream.string.substring(stream.pos).includes(this.query)) stream.pos += 1;
+          else stream.skipToEnd();
+          return undefined;
+        },
+      };
+      this.codemirror.addOverlay(this.overlay);
+    },
+    removeOverlay() { if (this.overlay) this.codemirror.removeOverlay(this.overlay); },
 
     selectNext() {
       this.cursor.findNext();
@@ -43,10 +65,16 @@ export default {
       if (this.query.length && this.hasMatches) {
         this.selectNext();
         this.codemirror.focus();
-      // Otherwise deselect everything
-      } else if (!this.query.length) {
-        this.deselect();
       }
     },
   },
+
+  watch: {
+    query(q) {
+      this.removeOverlay();
+      if (q) this.setOverlay();
+    },
+  },
+
+  destroyed() { this.removeOverlay(); },
 };
