22145a4db1859f3752b7f049f93dafd74906a9e8
Luke Taylor <luke@deentaylor.com>
2019-02-04 22:44:44 -0500

    Add async delay helper method

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -1,5 +1,7 @@
-/* eslint-disable import/prefer-default-export */
-
 export function clip(val, low, high) {
   return Math.max(Math.min(val, high), low);
+}
+
+export function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
