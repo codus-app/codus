c3313518f5ab481069643df0299f3a7761593976
Luke Taylor <luke@deentaylor.com>
2019-02-14 00:02:05 -0500

    Add large modal to profile picture upload component

--- app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
+++ app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="profile-picture-upload">
-    <div class="picture" v-bind:style="{ backgroundImage: `url(${url})` }">
+    <div class="picture" v-on:click="modalOpen = true" v-bind:style="{ backgroundImage: `url(${url})` }">
       <div class="center">
         <icon-camera></icon-camera>
         <div class="label">{{ hasDefaultPicture ? 'Upload' : 'Change'}}</div>
       </div>
     </div>
+
+    <modal v-bind:wide="true" v-bind:open="modalOpen" v-on:close="modalOpen = false" fade-color="#1e1e21">
+    </modal>
   </div>
 </template>
 
--- app/src/pages/settings/account/profile-picture-upload/script.js
+++ app/src/pages/settings/account/profile-picture-upload/script.js
@@ -1,6 +1,10 @@
 export default {
   props: { url: String },
 
+  data: () => ({
+    modalOpen: false,
+  }),
+
   computed: {
     hasDefaultPicture() {
       return this.url.startsWith('https://app.codus.io/static');
