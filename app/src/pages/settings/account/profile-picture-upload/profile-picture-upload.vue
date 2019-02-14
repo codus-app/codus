471e1cfb835972fd8c2b65ed14ff2cb30e3dcf72
Luke Taylor <luke@deentaylor.com>
2019-02-14 10:47:26 -0500

    Move rings into new modal

--- app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
+++ app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="profile-picture-upload">
-    <div
-      class="circle-modal"
-      v-bind:class="{ open: modalOpen }"
-    >
-      <div class="scrim" v-on:click="modalOpen = false"></div>
+    <div class="circle-modal" v-bind:class="{ open: modalOpen }">
       <div class="circle"></div>
+      <div class="scrim" v-on:click="modalOpen = false; dropOver = false; dropped = false;"></div>
+    </div>
+
+    <div class="upload">
+      <div
+        class="upload-window"
+        v-bind:class="{ open: modalOpen, 'drop-over': dropOver, dropped }"
+        v-on:dragenter="dropOver = true"
+        v-on:dragleave="dropOver = false"
+        v-on:drop="dropped = true"
+      >
+        <div class="rings">
+          <div class="ring" v-for="i in 4" v-bind:key="i"></div>
+        </div>
+
+        <icon-upload-cloud></icon-upload-cloud>
+      </div>
     </div>
 
     <div class="picture" v-on:click="modalOpen = true" v-bind:style="{ backgroundImage: `url(${url})` }">
