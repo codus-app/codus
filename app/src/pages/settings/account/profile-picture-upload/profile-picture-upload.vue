419a16f4326029efbe8f2bcf6a7a14193016b332
Luke Taylor <luke@deentaylor.com>
2019-02-14 10:18:49 -0500

    Replace upload modal with circular modal that expands from upload button

--- app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
+++ app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="profile-picture-upload">
+    <div
+      class="circle-modal"
+      v-bind:class="{ open: modalOpen }"
+    >
+      <div class="scrim" v-on:click="modalOpen = false"></div>
+      <div class="circle"></div>
+    </div>
+
     <div class="picture" v-on:click="modalOpen = true" v-bind:style="{ backgroundImage: `url(${url})` }">
       <div class="center">
         <icon-camera></icon-camera>
         <div class="label">{{ hasDefaultPicture ? 'Upload' : 'Change'}}</div>
       </div>
     </div>
-
-    <modal v-bind:wide="true" v-bind:open="modalOpen" v-on:close="modalOpen = false; dropOver = false; dropped = false;" fade-color="#1e1e21">
-      <div
-        class="upload-window"
-        v-bind:class="{ 'drop-over': dropOver, dropped }"
-        v-on:dragenter="dropOver = true"
-        v-on:dragleave="dropOver = false"
-        v-on:drop="dropped = true"
-      >
-        <!-- Rings -->
-        <div class="rings">
-          <div class="ring" v-for="i in 5" v-bind:key="i"></div>
-        </div>
-
-        <icon-upload-cloud></icon-upload-cloud>
-    </modal>
   </div>
 </template>
 
