79c00a863805311d537963787ee496920f2770d5
Luke Taylor <luke@deentaylor.com>
2019-02-20 23:19:34 -0500

    Tweaks to file uploader
    Set 'dragged' state while picking a file

--- app/src/pages/settings/account/profile-picture-upload/script.js
+++ app/src/pages/settings/account/profile-picture-upload/script.js
@@ -14,7 +14,18 @@ export default {
   },
 
   methods: {
-    browse() { this.$refs.fileInput.click(); },
+    browse() {
+      this.dropOver = true;
+      this.$refs.fileInput.click();
+      // Listen to events on body to try to detect when "cancel" has been clicked on file input and
+      // focus has returned to the document
+      const eventsToListen = ['onfocus', 'onmouseenter'];
+      const eventListener = () => {
+        eventsToListen.forEach((ename) => { document.body[ename] = null; });
+        this.dropOver = false;
+      };
+      eventsToListen.forEach((ename) => { document.body[ename] = eventListener; });
+    },
 
     handleFile(e) {
       const file = (e.dataTransfer || e.target).files[0];
