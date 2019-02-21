1ed7507353d2be238ebfa6e5fa4aaa46f4dca6f8
Luke Taylor <luke@deentaylor.com>
2019-02-20 23:00:57 -0500

    DRAMATICALLY cut back on the profile image upload UI :(

--- app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
+++ app/src/pages/settings/account/profile-picture-upload/profile-picture-upload.vue
@@ -1,55 +1,39 @@
 <template>
   <div class="profile-picture-upload">
-    <div class="circle-modal" v-bind:class="{ open: modalOpen }">
-      <div class="circle"></div>
-      <div class="scrim" v-on:click="close"></div>
-    </div>
-
-    <div class="upload">
-      <div
-        class="upload-window"
-        v-bind:class="{ open: modalOpen, 'drop-over': dropOver, 'has-dragged': hasDragged, dropped }"
-        v-on:dragenter="dropOver = true; hasDragged = true;"
-        v-on:dragleave="handleDragLeave"
-        v-on:drop="handleFile"
-        v-on:click="browse"
-      >
-        <div class="rings">
-          <div class="ring" v-for="i in 3" v-bind:key="i"></div>
-        </div>
-
-        <icon-upload-cloud></icon-upload-cloud>
-
-        <input
-          ref="fileInput"
-          type="file"
-          accept="image/png, image/jpeg, image/gif"
-          v-on:change="handleFile"
-        >
-
-        <div
-          class="new-picture"
-          v-bind:class="{ empty: !imageDataURL.length }"
-          v-bind:style="{ backgroundImage: imageDataURL ? `url(${imageDataURL})` : '' }"
-        ></div>
-
-        <div class="confirmation-buttons">
-          <button class="reject" v-on:click="$event.stopPropagation(); close();"><icon-x></icon-x></button>
-          <button class="accept" v-on:click="$event.stopPropagation()"><icon-check></icon-check></button>
-        </div>
-      </div>
-    </div>
+    <!-- Old picture (displayed with overlay) -->
 
     <div class="picture"
-      v-on:click="modalOpen = true; modalOpenedFromDrag = false;"
-      v-on:dragenter="modalOpen = true; modalOpenedFromDrag = true; dropOver = true; hasDragged = true;"
+      v-bind:class="{ dragging: dropOver, dropped: !!imageDataURL.length }"
+      v-on:click="browse"
+      v-on:dragenter="dropOver = true;"
+      v-on:dragleave="dropOver = false;"
+      v-on:drop="handleFile"
       v-bind:style="{ backgroundImage: `url(${url})` }"
     >
       <div class="center">
-        <icon-camera></icon-camera>
-        <div class="label">{{ hasDefaultPicture ? 'Upload' : 'Change'}}</div>
+        <!-- Default display: "change" CTA with camera icon -->
+        <div class="main">
+          <icon-camera></icon-camera>
+          <div class="label">{{ hasDefaultPicture ? 'Upload' : 'Change'}}</div>
+        </div>
+        <!-- When dragging, display a "cloud upload" icon -->
+        <icon-upload-cloud class="alt"></icon-upload-cloud>
       </div>
     </div>
+
+    <div
+      class="new-picture"
+      v-bind:class="{ empty: !imageDataURL.length }"
+      v-bind:style="{ backgroundImage: imageDataURL ? `url(${imageDataURL})` : '' }"
+    ></div>
+
+    <input
+      ref="fileInput"
+      type="file"
+      accept="image/png, image/jpeg, image/gif"
+      v-on:change="handleFile"
+    >
+
   </div>
 </template>
 
--- app/src/pages/settings/account/profile-picture-upload/script.js
+++ app/src/pages/settings/account/profile-picture-upload/script.js
@@ -1,14 +1,8 @@
-import { delay } from '../../../../helpers';
-
 export default {
   props: { url: String },
 
   data: () => ({
-    modalOpen: false, // Should the upload window be open or closed?
-    dropOver: false, // Is the user dragging a file over the upload window?
-    // These are reset when the upload window closes, so they only represent the current "session"
-    hasDragged: false, // Has the user dragged a file over the upload window in the past?
-    dropped: false, // Has the user dropped a file into the upload window?
+    dropOver: false, // Is the user dragging a file over the upload area?
 
     imageDataURL: '',
   }),
@@ -20,47 +14,15 @@ export default {
   },
 
   methods: {
-    handleDragLeave() {
-      if (this.modalOpenedFromDrag) return this.close();
-      this.dropOver = false;
-      return undefined;
-    },
-
-    browse() {
-      if (!this.dropped) this.$refs.fileInput.click();
-    },
+    browse() { this.$refs.fileInput.click(); },
 
     handleFile(e) {
-      if (this.dropped) return; // Don't allow replacing dropped file with a second one
-
-      this.dropped = true;
-
       const file = (e.dataTransfer || e.target).files[0];
       const reader = new FileReader();
       reader.addEventListener('load', (e2) => { this.imageDataURL = e2.target.result; });
       reader.readAsDataURL(file);
+
+      this.dropOver = false;
     },
-
-
-    close() {
-      Object.assign(this, {
-        modalOpen: false,
-        dropped: false,
-        hasDragged: false,
-      });
-
-      this.$nextTick().then(() => {
-        // Reset imageDataUrl either after 500ms or when the modal is re-opened
-        Promise.race([
-          delay(500),
-          new Promise((resolve) => { this.$once('modalOpen', resolve); }),
-        ])
-          .then(() => { this.imageDataURL = ''; this.dropOver = false; });
-      });
-    },
-  },
-
-  watch: {
-    modalOpen(open) { this.$emit(open ? 'modalOpen' : 'modalClose'); },
   },
 };
