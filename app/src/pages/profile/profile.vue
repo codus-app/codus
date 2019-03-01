d5096275590e5421eaf20def24ec68998537fba3
Luke Taylor <luke@deentaylor.com>
2019-03-01 12:08:57 -0500

    Layouts for top section of user profile page

--- app/src/pages/profile/profile.vue
+++ app/src/pages/profile/profile.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    test
+  <div class="page">
+    <div class="top">
+      <div class="summary card"></div>
+      <div class="detail card c1"></div>
+      <div class="detail card c2"></div>
+      <div class="detail card c3"></div>
+      <div class="detail card c4"></div>
+    </div>
   </div>
 </template>
 
