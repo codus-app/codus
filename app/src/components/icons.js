8ba64bcd445cb3171876f791fa9f1df70ac262ec
Luke Taylor <luke@deentaylor.com>
2019-03-03 18:30:50 -0500

    Alphabetize icon imports

--- app/src/components/icons.js
+++ app/src/components/icons.js
@@ -1,73 +1,62 @@
 import Vue from 'vue';
 
 import {
-  // Sidebar icons
-  HomeIcon,
-  UserIcon,
-  LogOutIcon,
-  // Home icons
-  CheckIcon,
-  // Editor icons
-  ChevronsLeftIcon,
-  MenuIcon,
-  PlayIcon,
-  Trash2Icon,
-  SearchIcon,
-  FolderIcon,
-  FileIcon,
-  FileTextIcon,
-  Minimize2Icon,
-  Maximize2Icon,
-  CopyIcon,
-  // Settings pages icons
-  SettingsIcon,
-  SlidersIcon,
-  EditIcon,
-  CameraIcon,
-  UploadCloudIcon,
-  // Social icons
   AtSignIcon,
-  // Misc
-  ChevronUpIcon,
-  ChevronRightIcon,
+  CameraIcon,
+  CheckIcon,
   ChevronDownIcon,
   ChevronLeftIcon,
-  XIcon,
-  XCircleIcon,
+  ChevronRightIcon,
+  ChevronsLeftIcon,
+  ChevronUpIcon,
+  CopyIcon,
+  EditIcon,
+  FileIcon,
+  FileTextIcon,
+  FolderIcon,
+  HomeIcon,
+  LogOutIcon,
+  Maximize2Icon,
+  MenuIcon,
+  Minimize2Icon,
   MoreHorizontalIcon,
+  PlayIcon,
+  SearchIcon,
+  SettingsIcon,
+  SlidersIcon,
+  Trash2Icon,
+  UploadCloudIcon,
+  UserIcon,
+  XCircleIcon,
+  XIcon,
 } from 'vue-feather-icons';
 
-// Sidebar icons
-Vue.component('icon-home', HomeIcon);
-Vue.component('icon-user', UserIcon);
-Vue.component('icon-log-out', LogOutIcon);
-// Home icons
-Vue.component('icon-check', CheckIcon);
-// Editor icons
-Vue.component('icon-chevrons-left', ChevronsLeftIcon);
-Vue.component('icon-menu', MenuIcon);
-Vue.component('icon-play', PlayIcon);
-Vue.component('icon-trash', Trash2Icon);
-Vue.component('icon-search', SearchIcon);
-Vue.component('icon-folder', FolderIcon);
-Vue.component('icon-file', FileIcon);
-Vue.component('icon-file-text', FileTextIcon);
-Vue.component('icon-maximize', Maximize2Icon);
-Vue.component('icon-minimize', Minimize2Icon);
-Vue.component('icon-copy', CopyIcon);
-// Settings pages icons
-Vue.component('icon-settings', SettingsIcon);
-Vue.component('icon-sliders', SlidersIcon);
-Vue.component('icon-edit', EditIcon);
-Vue.component('icon-camera', CameraIcon);
-Vue.component('icon-upload-cloud', UploadCloudIcon);
-// Social icons
+
 Vue.component('icon-at-sign', AtSignIcon);
-// Misc
-Vue.component('icon-chevron-up', ChevronUpIcon);
-Vue.component('icon-chevron-right', ChevronRightIcon);
+Vue.component('icon-camera', CameraIcon);
+Vue.component('icon-check', CheckIcon);
 Vue.component('icon-chevron-down', ChevronDownIcon);
 Vue.component('icon-chevron-left', ChevronLeftIcon);
-Vue.component('icon-x', XIcon);
-Vue.component('icon-x-circle', XCircleIcon);
+Vue.component('icon-chevron-right', ChevronRightIcon);
+Vue.component('icon-chevron-up', ChevronUpIcon);
+Vue.component('icon-chevrons-left', ChevronsLeftIcon);
+Vue.component('icon-copy', CopyIcon);
+Vue.component('icon-edit', EditIcon);
+Vue.component('icon-file-text', FileTextIcon);
+Vue.component('icon-file', FileIcon);
+Vue.component('icon-folder', FolderIcon);
+Vue.component('icon-home', HomeIcon);
+Vue.component('icon-log-out', LogOutIcon);
+Vue.component('icon-maximize', Maximize2Icon);
+Vue.component('icon-menu', MenuIcon);
+Vue.component('icon-minimize', Minimize2Icon);
 Vue.component('icon-more', MoreHorizontalIcon);
+Vue.component('icon-play', PlayIcon);
+Vue.component('icon-search', SearchIcon);
+Vue.component('icon-settings', SettingsIcon);
+Vue.component('icon-sliders', SlidersIcon);
+Vue.component('icon-trash', Trash2Icon);
+Vue.component('icon-upload-cloud', UploadCloudIcon);
+Vue.component('icon-user', UserIcon);
+Vue.component('icon-x-circle', XCircleIcon);
+Vue.component('icon-x', XIcon);
