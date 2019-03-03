d0283a5095446dae93003522848fe34bbf5a8482
Luke Taylor <luke@deentaylor.com>
2019-03-03 12:37:27 -0500

    Move icon component registration to a separate file

---
+++ app/src/components/icons.js
@@ -0,0 +1,69 @@
+import Vue from 'vue';
+
+import {
+  // Sidebar icons
+  HomeIcon,
+  UserIcon,
+  LogOutIcon,
+  // Home icons
+  CheckIcon,
+  // Editor icons
+  ChevronsLeftIcon,
+  MenuIcon,
+  PlayIcon,
+  Trash2Icon,
+  SearchIcon,
+  FolderIcon,
+  FileIcon,
+  FileTextIcon,
+  Minimize2Icon,
+  Maximize2Icon,
+  // Settings pages icons
+  SettingsIcon,
+  SlidersIcon,
+  CameraIcon,
+  UploadCloudIcon,
+  // Social icons
+  AtSignIcon,
+  // Misc
+  ChevronUpIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  XIcon,
+  XCircleIcon,
+  MoreHorizontalIcon,
+} from 'vue-feather-icons';
+
+// Sidebar icons
+Vue.component('icon-home', HomeIcon);
+Vue.component('icon-user', UserIcon);
+Vue.component('icon-log-out', LogOutIcon);
+// Home icons
+Vue.component('icon-check', CheckIcon);
+// Editor icons
+Vue.component('icon-chevrons-left', ChevronsLeftIcon);
+Vue.component('icon-menu', MenuIcon);
+Vue.component('icon-play', PlayIcon);
+Vue.component('icon-trash', Trash2Icon);
+Vue.component('icon-search', SearchIcon);
+Vue.component('icon-folder', FolderIcon);
+Vue.component('icon-file', FileIcon);
+Vue.component('icon-file-text', FileTextIcon);
+Vue.component('icon-maximize', Maximize2Icon);
+Vue.component('icon-minimize', Minimize2Icon);
+// Settings pages icons
+Vue.component('icon-settings', SettingsIcon);
+Vue.component('icon-sliders', SlidersIcon);
+Vue.component('icon-camera', CameraIcon);
+Vue.component('icon-upload-cloud', UploadCloudIcon);
+// Social icons
+Vue.component('icon-at-sign', AtSignIcon);
+// Misc
+Vue.component('icon-chevron-up', ChevronUpIcon);
+Vue.component('icon-chevron-right', ChevronRightIcon);
+Vue.component('icon-chevron-down', ChevronDownIcon);
+Vue.component('icon-chevron-left', ChevronLeftIcon);
+Vue.component('icon-x', XIcon);
+Vue.component('icon-x-circle', XCircleIcon);
+Vue.component('icon-more', MoreHorizontalIcon);
