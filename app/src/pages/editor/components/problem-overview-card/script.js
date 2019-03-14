e3c5e35736ae682a54726011bf1e4177c2abd04a
Luke Taylor <luke@deentaylor.com>
2019-03-14 16:40:27 -0400

    Add accidentally deleted gradient back to problem-overview-card

--- app/src/pages/editor/components/problem-overview-card/script.js
+++ app/src/pages/editor/components/problem-overview-card/script.js
@@ -5,6 +5,7 @@ export default {
     progressStyle() {
       return {
         width: `${this.progress * 100}%`,
+        background: `linear-gradient(90deg, #4DFB9B 0%, #1DB965 ${(1 / this.progress) * 100}%)`,
       };
     },
   },
