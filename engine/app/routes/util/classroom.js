50fbf6a6e573268363c9f259212b84134f4f5e55
Luke Taylor <luke@deentaylor.com>
2019-03-18 10:55:51 -0400

    Create method to generate unique invite codes for classes

---
+++ engine/app/routes/util/classroom.js
@@ -0,0 +1,20 @@
+const keystone = require('keystone');
+
+const Classroom = keystone.list('Classroom');
+
+module.exports.generateInviteCode = async function generateInviteCode(length = 6) {
+  const charset = 'abcdfghjklmnpqrstvwxyz123456789';
+
+  const buildRandomString = len => new Array(len).fill(null)
+    .map(() => charset[Math.floor(Math.random() * charset.length)])
+    .join('');
+
+  const code = buildRandomString(length);
+
+  const existingClass = await Classroom.model
+    .findOne()
+    .where('code').equals(code);
+
+  if (existingClass) return generateInviteCode(); // Prevent collisions
+  return code;
+};
