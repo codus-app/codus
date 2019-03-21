b61b08ca2d4a5c1a482ff3d3189e173941e2e793
Luke Taylor <luke@deentaylor.com>
2019-03-21 11:32:39 -0400

    Add action to instructor Vuex module for fetching classes from API

--- app/src/vuex/modules/instructor.js
+++ app/src/vuex/modules/instructor.js
@@ -1,3 +1,32 @@
+import * as api from '../../api';
+import store from '..';
+
 export default {
   namespaced: true,
+
+  state: {
+    classes: [],
+  },
+
+  mutations: {
+    classesFetched(state, payload) {
+      // An index of codes of classes that are already represented in Vuex
+      const storedCodes = state.classes.map(c => c.code);
+      // All of the classes that were fetched that were not previously represented in Vuex
+      const fresh = payload.filter(c => !storedCodes.includes(c.code));
+      state.classes = [
+        // Update already-stored classes with new info from the request
+        ...state.classes.map(c => ({ ...c, ...payload.find(c2 => c2.code === c.code) })),
+        // Add new classes
+        ...fresh,
+      ];
+    },
+  },
+
+  actions: {
+    async fetchClasses({ commit }) {
+      const classes = await api.get({ endpoint: '/classroom/classrooms', store });
+      commit('classesFetched', classes);
+    },
+  },
 };
