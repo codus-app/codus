2ada5c2167cdea3884402d9ae5b26f4ab01a1445
Luke Taylor <luke@deentaylor.com>
2019-03-21 11:55:00 -0400

    classROOMs not classes

--- app/src/vuex/modules/instructor.js
+++ app/src/vuex/modules/instructor.js
@@ -6,43 +6,43 @@ export default {
   namespaced: true,
 
   state: {
-    classes: [],
+    classrooms: [],
   },
 
   mutations: {
-    classesFetched(state, payload) {
-      // An index of codes of classes that are already represented in Vuex
-      const storedCodes = state.classes.map(c => c.code);
-      // All of the classes that were fetched that were not previously represented in Vuex
+    classroomsFetched(state, payload) {
+      // An index of codes of classrooms that are already represented in Vuex
+      const storedCodes = state.classrooms.map(c => c.code);
+      // All of the classrooms that were fetched that were not previously represented in Vuex
       const fresh = payload.filter(c => !storedCodes.includes(c.code));
-      state.classes = [
-        // Update already-stored classes with new info from the request
-        ...state.classes.map(c => ({ ...c, ...payload.find(c2 => c2.code === c.code) })),
-        // Add new classes
+      state.classrooms = [
+        // Update already-stored classrooms with new info from the request
+        ...state.classrooms.map(c => ({ ...c, ...payload.find(c2 => c2.code === c.code) })),
+        // Add new classrooms
         ...fresh,
       ];
     },
 
-    mutateClass(state, payload) {
-      const index = state.classes.findIndex(c => c.code === payload.code);
-      if (index < 0) state.classes = [...state.classes, payload];
-      else Vue.set(state.classes, index, { ...state.classes[index], ...payload });
+    mutateClassroom(state, payload) {
+      const index = state.classrooms.findIndex(c => c.code === payload.code);
+      if (index < 0) state.classrooms = [...state.classrooms, payload];
+      else Vue.set(state.classrooms, index, { ...state.classrooms[index], ...payload });
     },
   },
 
-  /** Fetch a list of all managed classes from the API */
+  /** Fetch a list of all managed classrooms from the API */
   actions: {
-    async fetchClasses({ commit }) {
-      const classes = await api.get({ endpoint: '/classroom/classrooms', store });
-      commit('classesFetched', classes);
+    async fetchClassrooms({ commit }) {
+      const classrooms = await api.get({ endpoint: '/classroom/classrooms', store });
+      commit('classroomsFetched', classrooms);
     },
 
-    /** Hydrate a stored class with additional info (i.e. students) that is only exposed when
-      * querying that class directly
+    /** Hydrate a stored classroom with additional info (i.e. students) that is only exposed when
+      * querying that classroom directly
       */
-    async fetchClass({ commit }, code) {
+    async fetchClassroom({ commit }, code) {
       const classroom = await api.get({ endpoint: `/classroom/${code}`, store });
-      commit('mutateClass', classroom);
+      commit('mutateClassroom', classroom);
     },
   },
 };
