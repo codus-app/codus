3524779f2e9457bcd0ab8e946e9e5e4d798ba78c
Luke Taylor <luke@deentaylor.com>
2019-03-20 22:59:11 -0400

    Add roleSwitch middleware
    Applies handler functions conditionally based on role

--- engine/app/routes/classroom/index.js
+++ engine/app/routes/classroom/index.js
@@ -2,6 +2,7 @@ const keystone = require('keystone');
 
 const User = keystone.list('User');
 
+/** Protect a route on the condition that the authenticated user has a given role */
 function enforceRole(role) {
   return async (req, res, next) => {
     req.user2 = await User.model
@@ -13,10 +14,24 @@ function enforceRole(role) {
   };
 }
 
+/** Use a different route handler depending on whether the authenticated user is a student or an
+  * instructor
+  */
+function roleSwitch(handlers) {
+  return async (req, res) => {
+    req.user2 = await User.model
+      .findOne()
+      .where('userId').equals(req.user.sub);
+
+    return handlers[req.user2.role](req, res);
+  };
+}
+
 
 /* eslint-disable global-require */
 module.exports = {
   enforceRole,
+  roleSwitch,
   student: require('./student'),
   instructor: require('./instructor'),
 };
