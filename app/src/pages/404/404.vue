1e1a2463a12e5de7cc9d352816b20e58e66ae955
Luke Taylor <luke@deentaylor.com>
2019-03-26 12:50:48 -0400

    Add fanciness to the 404 page

--- app/src/pages/404/404.vue
+++ app/src/pages/404/404.vue
@@ -3,7 +3,15 @@
     <div class="message">
       <h1>404</h1>
       <h3>There's nothing here!</h3>
-      <router-link to="/">Go home</router-link>
+      <router-link
+        to="/"
+        v-on:mouseenter.native="linkHovered = true"
+        v-on:mouseleave.native="linkHovered = false"
+      >Go home</router-link>
+    </div>
+
+    <div class="rings" v-bind:class="{ active: linkHovered }">
+      <div class="ring" v-for="i in 7" v-bind:key="i"></div>
     </div>
   </div>
 </template>
