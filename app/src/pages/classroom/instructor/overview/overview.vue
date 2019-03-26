d2a831682faea39c292177a72a7719fc74775941
Luke Taylor <luke@deentaylor.com>
2019-03-26 10:34:52 -0400

    Add students grid to classroom overview

--- app/src/pages/classroom/instructor/overview/overview.vue
+++ app/src/pages/classroom/instructor/overview/overview.vue
@@ -9,12 +9,21 @@
 
     <div class="row">
       <dashboard-card class="students">
-        <template slot="label">{{ (students || []).length || '' }} Students</template>
+        <template slot="label">{{ students.length || '' }} Students</template>
         <template slot="link">View all</template>
+        <ul class="students-list" v-if="students">
+          <li class="student" v-for="student in students.slice(0, 8)" v-bind:key="student.username">
+            <div
+              class="image"
+              v-bind:style="{ 'background-image': `url(${student.picture})` }"
+            ></div>
+            <div class="name">{{ student.name }}</div>
+          </li>
+        </ul>
       </dashboard-card>
 
       <dashboard-card class="assignments">
-        <template slot="label">{{ (assignments || []).length || '' }} Assignments</template>
+        <template slot="label">{{ assignments.length || '' }} Assignments</template>
         <template slot="link">View all</template>
       </dashboard-card>
     </div>
