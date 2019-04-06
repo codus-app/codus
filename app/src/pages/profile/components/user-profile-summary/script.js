e03596c8896a72790e19cc6189d950efaa9c7b2e
Luke Taylor <luke@deentaylor.com>
2019-04-06 11:30:46 -0400

    Fix profile image size issues

--- app/src/pages/profile/components/user-profile-summary/script.js
+++ app/src/pages/profile/components/user-profile-summary/script.js
@@ -1,9 +1,12 @@
+import ResizeSensor from 'css-element-queries/src/ResizeSensor';
+
 export default {
   props: ['profile'],
 
   mounted() {
     this.$refs.picture.addEventListener('load', this.layout);
     window.addEventListener('resize', this.layout);
+    this.resizeSensor = new ResizeSensor(this.$el, this.layout);
   },
   destroyed() { window.removeEventListener('resize', this.layout); },
 
