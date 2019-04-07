9074da568f97fb9473d803ebbfdb3a1b6cfb25b5
Luke Taylor <luke@deentaylor.com>
2019-04-06 21:50:17 -0400

    Add confirmation modal to student removal

--- app/src/pages/classroom/instructor/students/script.js
+++ app/src/pages/classroom/instructor/students/script.js
@@ -6,23 +6,11 @@ export default {
 
   data() {
     return {
+      studentRemoval: { open: false, student: null },
+
       profileContextItems: [
-        {
-          icon: 'external-link',
-          label: 'View full profile',
-          onclick: () => this.$router.push({
-            name: 'profile',
-            params: { username: this.$route.params.username },
-          }),
-        },
-        {
-          icon: 'user-x',
-          label: 'Remove',
-          onclick: () => this.removeUser({
-            classroom: this.classroom.code,
-            username: this.selectedStudent.username,
-          }),
-        },
+        { icon: 'external-link', label: 'View full profile', onclick: this.visitFullProfile },
+        { icon: 'user-x', label: 'Remove', onclick: this.startStudentRemoval },
       ],
     };
   },
@@ -37,13 +25,23 @@ export default {
     },
   },
 
-  methods: mapActions(['removeUser']),
+  methods: {
+    ...mapActions(['removeUser']),
+    visitFullProfile() {
+      this.$router.push({ name: 'profile', params: { username: this.$route.params.username } });
+    },
+    startStudentRemoval() {
+      this.studentRemoval.student = this.selectedStudent;
+      this.studentRemoval.open = true;
+    },
+  },
 
   watch: {
     students(students, oldStudents) {
       const usernames = students.map(s => s.username);
       const oldUsernames = oldStudents.map(s => s.username);
       const { username } = this.$route.params;
+      console.log('ye');
       if (username && oldUsernames.includes(username) && !usernames.includes(username)) {
         this.$router.replace({
           name: 'classroom-students',
