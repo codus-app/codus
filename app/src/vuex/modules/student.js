e7e6cd4f0df948724d139c5c3b49afed57e8bc99
Luke Taylor <luke@deentaylor.com>
2019-04-09 10:12:48 -0400

    Begin student Vuex module

--- app/src/vuex/modules/student.js
+++ app/src/vuex/modules/student.js
@@ -1,3 +1,27 @@
+import * as api from '../../api';
+import store from '..';
+
 export default {
   namespaced: true,
+
+  state: {
+    classroom: null,
+  },
+
+  mutations: {
+    classroomFetched(state, payload) {
+      // First time we're fetching, or the classroom we receive is somehow a different classroom
+      //  from the old one
+      if (!state.classroom || state.classroom.code !== payload.code) state.classroom = payload;
+      else state.classroom = { ...state.classroom, ...payload };
+    },
+  },
+
+  actions: {
+    /** Fetch the classroom the user is currently in */
+    async fetchClassroom({ commit }) {
+      const classroom = await api.get({ endpoint: '/classroom', store });
+      commit('classroomFetched', { ...classroom, fetched: true });
+    },
+  },
 };
