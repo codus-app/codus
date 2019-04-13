787a17d48aa5184b1b1b86ce6537e51fd4d69ec6
Luke Taylor <luke@deentaylor.com>
2019-04-13 15:15:38 -0400

    Add classroom empty state component

---
+++ app/src/pages/classroom/instructor/empty/empty.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="empty-message">
+    <img src="/static/illustrations/empty-clipboard.svg">
+    <slot name="title"><h2>There’s nothing here</h2></slot>
+    <p><slot></slot></p>
+    <div class="action" v-on:click="$emit('action-click')">
+      <icon-plus></icon-plus>
+      <slot name="cta"><span>Add</span></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="sass" src="./style.sass"></style>
