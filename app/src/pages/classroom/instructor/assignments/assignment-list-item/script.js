3d14bc4cd07e5a04ba6832362fc7ca501055e7fa
Luke Taylor <luke@deentaylor.com>
2019-04-24 11:39:19 -0400

    Tweak assignment date display function

--- app/src/pages/classroom/instructor/assignments/assignment-list-item/script.js
+++ app/src/pages/classroom/instructor/assignments/assignment-list-item/script.js
@@ -7,18 +7,19 @@ export default {
   computed: {
     dueDate() {
       if (!this.assignment.dueDate) return 'No due date';
-      const date = dayjs('2019-04-27 6:00 pm');
-      if (date.isBefore(Date.now())) return 'Overdue';
+      const date = dayjs(this.assignment.dueDate);
 
       const formatted = date.calendar(null, {
         sameDay: '[Due] h:mm A',
         nextDay: '[Due tomorrow]',
         nextWeek: '[Due] dddd',
+        lastDay: '[Due yesterday]',
+        lastWeek: '[Due last] dddd',
         sameElse: '[ELSE]',
       });
       if (formatted !== 'ELSE') return formatted;
       const thisYear = date.year() === dayjs(Date.now()).year();
-      return date.format(thisYear ? '[Due] MMM D' : '[Due] MMM D YYYY');
+      return date.format(thisYear ? '[Due] MMM D' : '[Due] MMM D[,] YYYY');
     },
   },
 };
