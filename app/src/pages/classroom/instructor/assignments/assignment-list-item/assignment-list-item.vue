5e98d139915dd8b7f6b69fe8563a4ed17db77b89
Luke Taylor <luke@deentaylor.com>
2019-04-23 22:27:10 -0400

    Basic markup + styles for assignment list item

--- app/src/pages/classroom/instructor/assignments/assignment-list-item/assignment-list-item.vue
+++ app/src/pages/classroom/instructor/assignments/assignment-list-item/assignment-list-item.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="assignment-list-item">
-    {{ assignment.name }}
+    <div class="icon"><icon-clipboard></icon-clipboard></div>
+    <div class="info">
+      <h2>{{ assignment.name }}</h2>
+      <div class="due">
+        <icon-calendar></icon-calendar>
+        {{ dueDate }}
+      </div>
+    </div>
+    <div class="actions">
+      <icon-more></icon-more>
+      <icon-menu class="reorder"></icon-menu>
+    </div>
   </div>
 </template>
 
