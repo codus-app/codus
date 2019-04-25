82b3338e1574d4a9aa11f442b62c6e3a2c09f9b0
Luke Taylor <luke@deentaylor.com>
2019-04-25 10:26:35 -0400

    Automatically set sortOrder in pre-save hook

--- engine/app/models/Assignment.js
+++ engine/app/models/Assignment.js
@@ -10,7 +10,7 @@ Assignment.add({
   dueDate: { type: Types.Datetime, initial: true },
   problems: { type: Types.Relationship, ref: 'Problem', many: true, initial: true, required: true },
   classroom: { type: Types.Relationship, ref: 'Classroom', initial: true, required: true },
-  sortOrder: { type: Types.Number },
+  sortOrder: { type: Types.Number, initial: true },
 });
 
 Assignment.schema.virtual('code').get(function shortCode() { return this._id.toString().substring(0, 8); });
@@ -19,6 +19,20 @@ Assignment.schema.virtual('numProblems').get(function countProblems() { return t
 
 Assignment.schema.virtual('createdAt').get(function creationTime() {
   return new Date(parseInt(this.code, 16) * 1000);
+});
+
+// For assignments with no sort order set, automatically set to 1 more than the max sort order
+// within the same classroom
+Assignment.schema.pre('save', async function setSortOrder(next) {
+  if (!this.sortOrder) {
+    const lastAssignment = await Assignment.model
+      .findOne()
+      .where('classroom').equals(this.classroom)
+      .sort('-sortOrder');
+    const maxIndex = (lastAssignment || { sortOrder: -1 }).sortOrder;
+    this.sortOrder = maxIndex + 1;
+  }
+  next();
 });
 
 Assignment.register();
