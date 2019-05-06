f09c080128a86eb9a396ffda0b2f34531938accd
Luke Taylor <luke@deentaylor.com>
2019-05-06 00:58:50 -0400

    Begin replacement calendar header

--- app/src/components/classroom/assignment-create/pages/3/page-3.vue
+++ app/src/components/classroom/assignment-create/pages/3/page-3.vue
@@ -4,6 +4,8 @@
     <p>Optionally, set a date and/or time at which this assignment is due.</p>
 
     <datepicker
+      ref="datepicker"
+
       inline
       full-month-name
       maximum-view="day"
@@ -12,7 +14,16 @@
       v-bind:value="date"
       v-on:input="$emit('update:date', $event)"
       v-bind:language="en"
-    ></datepicker>
+
+      v-on:changedMonth="calendarPageChanged"
+    >
+      <div v-if="mounted" class="replacement-header" slot="beforeCalendarHeader">
+        <span class="month-name">
+          {{ datepicker.utils.getMonthName(monthDisplayed.getMonth(), en.months) }}
+        </span>
+        <span class="year-number">{{ monthDisplayed.getFullYear() }}</span>
+      </div>
+    </datepicker>
 
     <proceed-button v-bind:disabled="false" v-on:click="$emit('next')">
       <template slot="label">Finish up</template>
--- app/src/components/classroom/assignment-create/pages/3/script.js
+++ app/src/components/classroom/assignment-create/pages/3/script.js
@@ -8,8 +8,26 @@ export default {
 
   data: () => ({
     en,
+    mounted: false,
     today: new Date(now.getFullYear(), now.getMonth(), now.getDate()),
+    monthDisplayed: now,
   }),
+
+  computed: {
+    datepicker() {
+      return this.mounted && this.$refs.datepicker;
+    },
+  },
+
+  methods: {
+    calendarPageChanged(e) {
+      this.monthDisplayed = e;
+    },
+  },
+
+  mounted() {
+    this.mounted = true;
+  },
 
   components: {
     'proceed-button': require('../../proceed-button/proceed-button.vue').default,
