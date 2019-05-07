358f45ac354b4f9b57f61a85b7e81ae72d0b5475
Luke Taylor <luke@deentaylor.com>
2019-05-07 10:14:03 -0400

    Add function to tag a number with an ordinal suffix

--- app/src/helpers.js
+++ app/src/helpers.js
@@ -27,3 +27,12 @@ export function formatDueDate(dueDate) {
   const thisYear = date.year() === dayjs(Date.now()).year();
   return date.format(thisYear ? '[Due] MMM D' : '[Due] MMM D[,] YYYY');
 }
+
+export function ordinal(num) {
+  const ones = num % 10;
+  const tens = Math.floor((num % 100) / 10);
+  if (ones === 1 && tens !== 1) return `${num}st`;
+  if (ones === 2 && tens !== 1) return `${num}nd`;
+  if (ones === 3 && tens !== 1) return `${num}rd`;
+  return `${num}th`;
+}
