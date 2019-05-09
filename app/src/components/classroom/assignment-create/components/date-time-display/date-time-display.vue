a1525f7fd21b8a72fbbe3f3f401655cc6cd10a1e
Luke Taylor <luke@deentaylor.com>
2019-05-08 23:03:38 -0400

    Initial (flawed) date-time-picker implementation

--- app/src/components/classroom/assignment-create/components/date-time-display/date-time-display.vue
+++ app/src/components/classroom/assignment-create/components/date-time-display/date-time-display.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="date-time-display">
+    <select v-model="month">
+      <option value="0">January</option>
+      <option value="1">February</option>
+      <option value="2">March</option>
+      <option value="3">April</option>
+      <option value="4">May</option>
+      <option value="5">June</option>
+      <option value="6">July</option>
+      <option value="7">August</option>
+      <option value="8">September</option>
+      <option value="9">October</option>
+      <option value="10">November</option>
+      <option value="11">December</option>
+    </select>
+    <input type="text" v-model="day">
 
+    <span class="sep">at</span>
+
+    <input type="text" v-model="hours"> : <input type="text" v-model="minutes">
+    <div class="toggle"
+      v-on:click="period = ({ 'am': 'pm', 'pm': 'am' })[period]"
+    >{{ period }}</div>
   </div>
 </template>
 
--- app/src/components/classroom/assignment-create/components/date-time-display/script.js
+++ app/src/components/classroom/assignment-create/components/date-time-display/script.js
@@ -1,3 +1,35 @@
 export default {
+  props: { value: Date },
 
+  methods: {
+    date() { return new Date((this.value || new Date()).getTime()); },
+
+    twentyfour(hours, period) {
+      if (parseInt(hours, 10) === 12) return ({ am: 0, pm: 12 })[period];
+      return parseInt(hours, 10) + ({ am: 0, pm: 12 })[period];
+    },
+  },
+
+  computed: {
+    month: {
+      get() { return this.date().getMonth(); },
+      set(value) { this.$emit('input', new Date(this.date().setMonth(value))); },
+    },
+    day: {
+      get() { return this.date().getDate(); },
+      set(value) { this.$emit('input', new Date(this.date().setDate(value))); },
+    },
+    hours: {
+      get() { return this.date().getHours() % 12; },
+      set(value) { this.$emit('input', new Date(this.date().setHours(this.twentyfour(value, this.period)))); },
+    },
+    minutes: {
+      get() { return this.date().getMinutes().toString().padStart(2, '0'); },
+      set(value) { this.$emit('input', new Date(this.date().setMinutes(value))); },
+    },
+    period: {
+      get() { return (this.date().getHours() >= 12) ? 'pm' : 'am'; },
+      set(value) { this.$emit('input', new Date(this.date().setHours(this.twentyfour(this.hours, value)))); },
+    },
+  },
 };
