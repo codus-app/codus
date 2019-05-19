ef234920f5815427188dd5cb86d992f81ea76377
Luke Taylor <luke@deentaylor.com>
2019-05-19 11:07:31 -0400

    Fancier tooltips for collapsed sidebar links

--- app/src/components/sidebar/sidebar-link.vue
+++ app/src/components/sidebar/sidebar-link.vue
@@ -2,7 +2,15 @@
   <router-link
     class="sb-link" v-bind:class="{ collapsed, disabled, 'loose-match': meta.looseMatch }"
     v-bind:to="replaceParams(path)"
-    v-bind:title="collapsed ? meta.label : ''"
+    v-bind:title="meta.label"
+    v-tippy="{
+      placement: 'right',
+      distance: '15',
+      arrowTransform: 'scale(.85)',
+      trigger: 'manual',
+    }"
+    v-on:mouseenter.native="() => { if (collapsed && $el._tippy) $el._tippy.show(); }"
+    v-on:mouseleave.native="() => { if ($el._tippy) $el._tippy.hide(); }"
   >
     <!-- Selection indicator (highlights active route) -->
     <div class="indicator" v-if="!collapsed"></div>
@@ -17,6 +25,10 @@
 export default {
 
   props: ['path', 'meta', 'collapsed', 'disabled', 'replaceParams'],
+
+  watch: {
+    collapsed() { if (!this.collapsed && this.$el._tippy) this.$el._tippy.hide(); },
+  },
 
 };
 </script>
