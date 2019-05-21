d73df36baa126f11c159f495f213a8f2b7590acb
Luke Taylor <luke@deentaylor.com>
2019-05-21 10:15:17 -0400

    Fetch assignment in single assignment page

--- app/src/pages/classroom/instructor/assignment/script.js
+++ app/src/pages/classroom/instructor/assignment/script.js
@@ -1,3 +1,27 @@
+import { createNamespacedHelpers } from 'vuex';
+const { mapActions } = createNamespacedHelpers('classroom/instructor');
+
 export default {
   props: { classroom: Object },
+
+  computed: {
+    classroomFetched() { return this.classroom.fetched || false; },
+    id() { return this.$route.params.assignmentId; },
+    assignment() {
+      if (!this.classroomFetched) return false;
+      return this.classroom.assignments.find(({ id }) => id === this.id);
+    },
+  },
+
+  methods: {
+    ...mapActions(['fetchAssignment']),
+  },
+
+  async created() {
+    if (!this.classroomFetched) await new Promise(resolve => this.$watch('classroomFetched', (f) => { if (f) resolve(); }));
+    this.fetchAssignment({
+      classroom: this.classroom.code,
+      id: this.id,
+    });
+  },
 };
