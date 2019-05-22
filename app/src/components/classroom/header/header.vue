2adc1984c82bdcece1cbaf34dba53756ac1b461a
Luke Taylor <luke@deentaylor.com>
2019-05-22 12:28:35 -0400

    Make header h1 into a link to classroom home

--- app/src/components/classroom/header/header.vue
+++ app/src/components/classroom/header/header.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="classroom-header">
-    <h1 v-bind:class="{ 'has-subtitle': $slots.subtitle }">{{ classroom.name }}</h1>
+    <h1 v-bind:class="{ 'has-subtitle': $slots.subtitle }">
+      <router-link v-bind:to="{ name: 'classroom-overview', params: $route.params }">
+        {{ classroom.name }}
+      </router-link>
+    </h1>
     <h2 v-if="$slots.subtitle"><slot name="subtitle"></slot></h2>
     <div class="actions">
       <slot name="actions"></slot>
