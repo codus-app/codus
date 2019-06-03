ba3306c173a392eea04e30d9a32b746fac49a415
Luke Deen Taylor <luke@deentaylor.com>
2019-06-02 20:11:20 -0400

    Add hover state to solved modal

--- app/src/pages/editor/components/solved-modal/solved-modal.vue
+++ app/src/pages/editor/components/solved-modal/solved-modal.vue
@@ -8,10 +8,15 @@
           backgroundColor: '#f7f7f7',
           color: '#2e2e33',
           width: '14rem',
-          padding: '10rem 1.25rem 1.5rem',
+          padding: '10rem 1.25rem 1.75rem',
         }"
         fade-color="rgba(12, 12, 12, .65)"
       >
+        <h2>Congratulations!</h2>
+        <p>You’ve solved this problem. Don’t stop now; move on to the next one!</p>
+        <router-link class="onward" v-bind:to="nextUrl">Onward!</router-link>
+
+
         <div class="illustration">
           <div class="center">
             <icon-check-circle></icon-check-circle>
@@ -25,9 +30,6 @@
             <div class="cutout"></div>
           </div>
         </div>
-        <h2>Congratulations!</h2>
-        <p>You’ve solved this problem. Don’t stop now; move on to the next one!</p>
-        <router-link class="onward" v-bind:to="nextUrl">Onward!</router-link>
       </modal>
   </portal>
 </template>
